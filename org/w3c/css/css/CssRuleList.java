c3526edbbf22530934265cc348d4e1598b515449
Yves Lafon <ylafon@w3.org>
2003-10-20 12:52:17 +0000

    optimized by using StringBuffer instead of String concatenation. Save cpu and memory

--- org/w3c/css/css/CssRuleList.java
+++ org/w3c/css/css/CssRuleList.java
@@ -1,4 +1,7 @@
-/* Made by Sijtsche de Jong */
+// $Id$
+// Author: Sijtsche de Jong
+// (c) COPYRIGHT MIT, ERCIM and Keio, 2003.
+// Please first read the full copyright statement in file COPYRIGHT.html
 
 package org.w3c.css.css;
 
@@ -30,65 +33,68 @@ public class CssRuleList {
     }
 
     public void addAtRule(AtRule atRule) {
-		this.atRule = atRule;
-		atRuleString = atRule.toString();
+	this.atRule = atRule;
+	atRuleString = atRule.toString();
     }
 
     public String getAtRule() {
-		return atRuleString;
+	return atRuleString;
     }
 
     public String toString() {
-	String ret = new String();
-
+	StringBuffer ret = new StringBuffer();
+	
 	if (atRule.isEmpty()) {
-
-		if (null != atRule && !atRuleString.equals("")) {
-		    ret += atRuleString + " \n";
-		}
-
+	    if (null != atRule && !atRuleString.equals("")) {
+		ret.append(atRuleString);
+		ret.append(' ');
+		ret.append('\n');
+	    }
 	} else {
+	    if (!atRuleString.equals("")) {
+		ret.append(atRuleString);
+		ret.appent(' ');
+		ret.append('{');
+		ret.append('\n');
+		indent = "   ";
+	    }
+	    for (int i = 0; i < rulelist.size() ; i++ ) {
+		ret.append(indent);
+		ret.append(((CssStyleRule)rulelist.elementAt(i)).toString());
+	    }
 
-		if (!atRuleString.equals("")) {
-		    ret += atRuleString + " {\n";
-		    indent = "   ";
-		}
-		for (int i = 0; i < rulelist.size() ; i++ ) {
-		    ret += indent + ((CssStyleRule)rulelist.elementAt(i)).toString();
-		}
-
-		if (!atRuleString.equals("")) {
-		    ret += "}\n";
-		}
+	    if (!atRuleString.equals("")) {
+		ret.append('}');
+		ret.append('\n');
+	    }
 	}
-	return ret;
+	return ret.toString();
     }
 
     public String toHTML() {
-	String ret = new String();
+	StringBuffer ret = new StringBuffer();
 
 	if (null != atRule && atRule.isEmpty()) {
-
-		if (!atRuleString.equals("")) {
-		    ret += "<li><span class='atSelector'>" + atRuleString + "</span></li> \n\n";
-		}
-
+	    if (!atRuleString.equals("")) {
+		ret.append("<li><span class='atSelector'>");
+		ret.append(atRuleString);
+		ret.append("</span></li> \n\n");
+	    }
 	} else {
+	    if (!atRuleString.equals("")) {
+		ret.append("<li><span class='atSelector'>");
+		ret.append(atRuleString);
+		ret.append("</span> {\n<ul>\n");
+	    }
+	    for (int i = 0; i < rulelist.size() ; i++ ) {
+		ret.append(((CssStyleRule)rulelist.elementAt(i)).toHTML());
+	    }
 
-		if (!atRuleString.equals("")) {
-		    ret += "<li><span class='atSelector'>" + atRuleString + "</span> {\n" +
-			"<ul>\n";
-		}
-		for (int i = 0; i < rulelist.size() ; i++ ) {
-		    ret += ((CssStyleRule)rulelist.elementAt(i)).toHTML();
-		}
-
-		if (!atRuleString.equals("")) {
-		    ret += "</ul>}</li>\n";
-		}
+	    if (!atRuleString.equals("")) {
+		ret.append("</ul>}</li>\n");
+	    }
 	}
-
-	return ret;
+	return ret.toString();
     }
 
     public void clear() {
