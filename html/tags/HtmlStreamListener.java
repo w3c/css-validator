bb75acca1a0f10d941670cbb495db48da8a50cc0
Yves Lafon <ylafon@w3.org>
2007-04-24 11:32:04 +0000

    removed in favor of new html parser

--- html/tags/HtmlStreamListener.java
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 1997 by Groupe Bull.  All Rights Reserved */
-/* $Id$ */
-/* Author: Jean-Michel.Leon@sophia.inria.fr */
-
-package html.tags;
-
-public interface HtmlStreamListener {
-    public void notifyActivity(int lines, long bytes);
-}
