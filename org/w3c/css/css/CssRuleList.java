572c1221e890bc3865f55a1d916008009756d6ed
GRAND-MOURCEL Julien <J.GRANDMOURAL-prestataire@alptis.fr>
2007-07-30 11:35:52 +0000

    Removing the toHTML and clear functions (not used anymore thanks to Velocity)

--- org/w3c/css/css/CssRuleList.java
+++ org/w3c/css/css/CssRuleList.java
@@ -8,7 +8,6 @@ package org.w3c.css.css;
 import java.util.Vector;
 
 import org.w3c.css.parser.AtRule;
-import org.w3c.css.util.Util;
 
 public class CssRuleList {
 
@@ -65,62 +64,4 @@ public class CssRuleList {
 		return ret.toString();
 	}
 
-	/*
-	 * public String toString() { StringBuffer ret = new StringBuffer();
-	 * 
-	 * if (atRule == null || atRule.isEmpty()) { if (null != atRule &&
-	 * !atRuleString.equals("")) { ret.append(atRuleString); ret.append(' ');
-	 * ret.append('\n'); } } else { if (!atRuleString.equals("")) {
-	 * ret.append(atRuleString); ret.append(' '); ret.append('{');
-	 * ret.append('\n'); indent = " "; } for (int i = 0; i < rulelist.size() ;
-	 * i++ ) { ret.append(indent);
-	 * ret.append(((CssStyleRule)rulelist.elementAt(i)).toString()); }
-	 * 
-	 * if (!atRuleString.equals("")) { ret.append('}'); ret.append('\n'); } }
-	 * return ret.toString(); }
-	 */
-
-	/*
-	 * public String toHTML() { StringBuffer ret = new StringBuffer();
-	 * 
-	 * if (null != atRule && atRule.isEmpty()) { if (!atRuleString.equals("")) {
-	 * ret.append("<li><span class='atSelector'>"); ret.append(atRuleString);
-	 * ret.append("</span></li> \n\n"); } } else { if
-	 * (!atRuleString.equals("")) { ret.append("<li><span
-	 * class='atSelector'>"); ret.append(atRuleString); ret.append("</span> {\n<ul>\n"); }
-	 * for (int i = 0; i < rulelist.size() ; i++ ) {
-	 * ret.append(((CssStyleRule)rulelist.elementAt(i)).toHTML()); }
-	 * 
-	 * if (!atRuleString.equals("")) { ret.append("</ul>}</li>\n"); } }
-	 * return ret.toString(); }
-	 */
-	public String toHTML() {
-		String ret = "\t\t\t\t\t";
-		if (null != atRule && atRule.isEmpty()) {
-			if (!atRuleString.equals("")) {
-				ret += "<div class='atRule'><span class='atSelector'>";
-				ret += Util.escapeHTML(atRuleString);
-				ret += "</span></div> \n\n";
-			}
-		} else {
-			if (!atRuleString.equals("")) {
-				ret += "<div class='atRule'><span class='atSelector'>";
-				ret += Util.escapeHTML(atRuleString);
-				ret += "</span> {\n\t\t\t\t\t\t<div>\n";
-			}
-			for (int i = 0; i < rulelist.size(); i++) {
-				ret += ((CssStyleRule) rulelist.elementAt(i)).toHTML();
-			}
-			if (!atRuleString.equals("")) {
-				ret += "\t\t\t\t\t\t</div>}\n\t\t\t\t\t</div>\n";
-			}
-		}
-		return ret;
-	}
-
-	public void clear() {
-		atRuleString = "";
-		rulelist.removeAllElements();
-		pseudopage = "";
-	}
 }
--- org/w3c/css/css/CssStyleRule.java
+++ org/w3c/css/css/CssStyleRule.java
@@ -8,7 +8,6 @@ package org.w3c.css.css;
 import java.util.Vector;
 
 import org.w3c.css.properties.css1.CssProperty;
-import org.w3c.css.util.Util;
 
 public class CssStyleRule {
 
@@ -16,6 +15,21 @@ public class CssStyleRule {
 		this.selectors = selectors;
 		this.properties = properties;
 		this.indent = indent;
+	}
+	/**
+	 * This function is only used inside the velocity template
+	 * @return the list of selectors in a string
+	 */
+	public String getSelectors() {
+		return selectors;
+	}
+	
+	/**
+	 * This function is only used inside the velocity template
+	 * @return the list of properties in a Vector
+	 */
+	public Vector getProperties() {
+		return properties;
 	}
 
 	public String toString() {
@@ -67,33 +81,6 @@ public class CssStyleRule {
 	 * Some identation (\t) was necessary to maintain the correct formatting
 	 * of the html output.
 	 */
-	public String toHTML() {
-		String ret = "";
-		if (properties != null) {
-			ret = "\n\t\t\t\t<div class='selector'>\n\t\t\t\t\t<span class='selectorValue'>";
-			if (selectors != null) {
-				ret += selectors;
-				ret += "</span> {\n\t\t\t\t\t<div class='RuleList'>\n";
-			}
-
-			for (int i = 0; i < properties.size(); i++) {
-				CssProperty property = (CssProperty) properties.elementAt(i);
-				ret += "\t\t\t\t\t\t<div class='Rule'>";
-				ret += "<span class='Property'>";
-				ret += Util.escapeHTML(property.getPropertyName());
-				ret += "</span>";
-				ret += " : <span class='PropertyValue'>";
-				ret += Util.escapeHTML(property.toString());
-				ret += "</span>";
-				if (property.getImportant()) {
-					ret += " !important";
-				}
-				ret += ";</div>\n";
-			}
-			ret += "\t\t\t\t\t</div>}\n\t\t\t\t</div>\n\n";
-		}
-		return ret;
-	}
 
 	private String indent;
 	private String selectors;
