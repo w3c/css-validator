b22927250f954c385d7aaf20185759a1974cbf44
Olivier Thereaux <olivier.thereaux@bbc.co.uk>
2008-03-12 15:53:35 +0000

    class loading

--- autotest/autotest.sh
+++ autotest/autotest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp .:/usr/share/java/jaxp-1.2.jar:/usr/share/java/xercesImpl.jar:/usr/share/java/xmlParserAPIs.jar:/usr/local/Jigsaw/classes/jigsaw.jar AutoTest $1
+java -cp .:/usr/share/java/jaxp-1.2.jar:/usr/share/java/xercesImpl.jar:/usr/share/java/xmlParserAPIs.jar:/usr/local/Jigsaw/classes/jigsaw.jar autotest.AutoTest $1
