20e0ae21033e99aa70c6c8061c3b16ce6ad548de
Yves Lafon <ylafon@w3.org>
2009-02-14 09:03:43 +0000

    reformat

--- org/w3c/css/css/NewStyleSheet.java
+++ org/w3c/css/css/NewStyleSheet.java
@@ -16,113 +16,113 @@ import org.w3c.css.util.Warnings;
 
 public class NewStyleSheet {
 
-	String uri;
-	String title;
-	private Errors errors;
-	private Warnings warnings;
+    String uri;
+    String title;
+    private Errors errors;
+    private Warnings warnings;
 
-	/**
-	 * Add some errors to this style.
-	 * 
-	 * @param errors
-	 *            Some errors.
-	 */
-	public void addErrors(Errors errors) {
-		if (errors.getErrorCount() != 0) {
-			getErrors().addErrors(errors);
-		}
+    /**
+     * Add some errors to this style.
+     * 
+     * @param errors
+     *            Some errors.
+     */
+    public void addErrors(Errors errors) {
+	if (errors.getErrorCount() != 0) {
+	    getErrors().addErrors(errors);
 	}
+    }
 
-	/**
-	 * Add some warnings to this style.
-	 * 
-	 * @param warnings
-	 *            Some warnings.
-	 */
-	public void addWarnings(Warnings warnings) {
-		if (warnings.getWarningCount() != 0)
-			getWarnings().addWarnings(warnings);
+    /**
+     * Add some warnings to this style.
+     * 
+     * @param warnings
+     *            Some warnings.
+     */
+    public void addWarnings(Warnings warnings) {
+	if (warnings.getWarningCount() != 0)
+	    getWarnings().addWarnings(warnings);
+    }
+
+    /**
+     * Returns all errors.
+     */
+    public final Errors getErrors() {
+	return errors;
+    }
+
+    /**
+     * Returns all warnings.
+     */
+    public final Warnings getWarnings() {
+	return warnings;
+    }
+
+    static Vector atRuleList = new Vector(); // contains all @rules
+
+    // (CssRuleLists)
+
+    public void newAtRule(AtRule atRule) {
+	CssRuleList rulelist = new CssRuleList();
+	rulelist.addAtRule(atRule);
+	atRuleList.addElement(rulelist);
+	indent = "   ";
+    }
+
+    public void endOfAtRule() {
+	CssRuleList rulelist = new CssRuleList();
+	atRuleList.addElement(rulelist); // for the new set of rules
+	pseudopage = "";
+	important = false;
+	selectortext = "";
+	indent = "";
+    }
+
+    public void setImportant(boolean important) {
+	this.important = important;
+    }
+
+    public void setSelectorList(Vector selectors) {
+	String slave = selectors.toString();
+	slave = slave.substring(slave.indexOf("[") + 1, slave.indexOf("]"));
+	selectortext = slave;
+    }
+
+    public void setProperty(Vector properties) {
+	this.properties = properties;
+    }
+
+    public void endOfRule() {
+	CssStyleRule stylerule = new CssStyleRule(indent, selectortext,
+						  properties, important);
+	CssRuleList rulelist;
+	if (!atRuleList.isEmpty()) {
+	    rulelist = (CssRuleList) atRuleList.lastElement();
+	    useless = atRuleList.removeElement(rulelist);
+	} else {
+	    rulelist = new CssRuleList();
 	}
+	rulelist.addStyleRule(stylerule);
+	atRuleList.addElement(rulelist);
+    }
 
-	/**
-	 * Returns all errors.
-	 */
-	public final Errors getErrors() {
-		return errors;
-	}
+    public void pseudoPage(String name) {
+	pseudopage = name;
+    }
 
-	/**
-	 * Returns all warnings.
-	 */
-	public final Warnings getWarnings() {
-		return warnings;
-	}
+    public Vector getRules() {
+	return atRuleList;
+    }
 
-	static Vector atRuleList = new Vector(); // contains all @rules
+    static String pseudopage;
 
-	// (CssRuleLists)
+    static String selectortext;
 
-	public void newAtRule(AtRule atRule) {
-		CssRuleList rulelist = new CssRuleList();
-		rulelist.addAtRule(atRule);
-		atRuleList.addElement(rulelist);
-		indent = "   ";
-	}
+    static boolean important;
 
-	public void endOfAtRule() {
-		CssRuleList rulelist = new CssRuleList();
-		atRuleList.addElement(rulelist); // for the new set of rules
-		pseudopage = "";
-		important = false;
-		selectortext = "";
-		indent = "";
-	}
+    static Vector properties;
 
-	public void setImportant(boolean important) {
-		this.important = important;
-	}
+    boolean useless;
 
-	public void setSelectorList(Vector selectors) {
-		String slave = selectors.toString();
-		slave = slave.substring(slave.indexOf("[") + 1, slave.indexOf("]"));
-		selectortext = slave;
-	}
-
-	public void setProperty(Vector properties) {
-		this.properties = properties;
-	}
-
-	public void endOfRule() {
-		CssStyleRule stylerule = new CssStyleRule(indent, selectortext,
-				properties, important);
-		CssRuleList rulelist;
-		if (!atRuleList.isEmpty()) {
-			rulelist = (CssRuleList) atRuleList.lastElement();
-			useless = atRuleList.removeElement(rulelist);
-		} else {
-			rulelist = new CssRuleList();
-		}
-		rulelist.addStyleRule(stylerule);
-		atRuleList.addElement(rulelist);
-	}
-
-	public void pseudoPage(String name) {
-		pseudopage = name;
-	}
-
-	public Vector getRules() {
-		return atRuleList;
-	}
-
-	static String pseudopage;
-
-	static String selectortext;
-
-	static boolean important;
-
-	static Vector properties;
-
-	boolean useless;
-
-	static String indent = new String();
+    static String indent = new String();
 }
