5ad4503f3ccc3e4b8ff27b781229edfd370030e2
Olivier Thereaux <olivier.thereaux@bbc.co.uk>
2009-03-24 20:10:21 +0000

    enabling bulgarian translation for tests

--- org/w3c/css/util/Messages.java
+++ org/w3c/css/util/Messages.java
@@ -430,6 +430,22 @@ public class Messages {
 	}
 
 	// -----------------------
+	try {
+	    java.io.InputStream f = Messages.class.getResourceAsStream("Messages.properties.bg");
+	    try {
+		tmp = new Utf8Properties<String,String>();
+		tmp.load(f);
+		languages_name.add("bg");
+		languages.put("bg", tmp);
+	    } finally {
+		f.close();
+	    }
+	} catch (Exception e) {
+	    System.err.println("org.w3c.css.util.Messages: " + "couldn't load properties bg");
+	    System.err.println("  " + e.toString());
+	}
+
+	// -----------------------
 
 	try {
 	    java.io.InputStream f = Messages.class.getResourceAsStream("Messages.properties.zh-cn");
