f7ff6f11a2ccb2b5fda4db84a86bcf1fffdebe12
Yves Lafon <ylafon@w3.org>
2011-04-07 14:42:21 +0000

    *** empty log message ***

---
+++ validator.html.el
@@ -0,0 +1,467 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="el" lang="el" dir="ltr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Script-Type" content="text/javascript" />
+    <meta http-equiv="Content-Style-Type" content="text/css" />
+    <title>Υπηρεσία Ελέγχου Εγκυρότητας W3C CSS</title>
+    <link rev="made" href="mailto:www-validator-css@w3.org" />
+    <link rev="start" href="./" title="Αρχική Σελίδα της W3C CSS Υπηρεσίας Ελέγχου Εγκυρότητας" />
+    <style type="text/css" media="all">
+      @import "style/base.css";
+	      </style>   
+      <script type="text/javascript" src="scripts/mootools.js"></script>
+    <script type="text/javascript" src="scripts/w3c-validator.js"></script>
+    </head>
+
+  <body>
+   <div id="banner">
+    <h1 id="title"><a href="http://www.w3.org/"><img alt="W3C" width="110" height="61" id="logo" src="./images/w3c.png" /></a>
+    <a href="./"><span>CSS Validation Service</span></a></h1>
+    <p id="tagline">
+      Έλεγχος Cascading Style Sheets (CSS) και αρχείων (X)HTML με style sheets
+    </p>
+   </div>
+  <div id="frontforms">
+      <ul id="tabset_tabs">
+        <li><a href="#validate-by-uri" class="active">Με URI</a></li><li><a href="#validate-by-upload">Με μεταφορά αρχείου</a></li><li><a href="#validate-by-input">Με άμεση εισαγωγή</a></li>
+      </ul>
+      <div id="fields">
+
+      <fieldset id="validate-by-uri" class="tabset_content front">
+        <legend class="tabset_label">Έλεγχος εγκυρότητας με URI</legend>
+        <form method="get" action="validator">
+        <p class="instructions">
+          Εισάγετε το URI του αρχείου (HTML με CSS ή μόνο CSS) που θέλετε να ελέγξετε την εγκυρότητα :
+        </p>
+        <p>
+           <label title="Διεύθυνση σελίδας για Έλεγχο Εγκυρότητας" for="uri">Διεύθυνση :
+             <input type="text" name="uri" id="uri" size="45" />
+           </label>
+        </p>
+              <fieldset id="extra_opt_uri" class="moreoptions">
+    <legend class="toggletext" title="Show/Hide extra validation options">Περισσότερες επιλογές</legend>
+    <div class="options">
+    <table>
+    <tr>
+    <th id="header_profile_uri">
+      <label for="profile_uri">Προφίλ :</label>
+    </th>
+    <td headers="header_profile_uri">
+      <select id="profile_uri" name="profile">
+        <option value="none">Κανένα ειδικό προφίλ</option>
+        <option value="css1">Επίπεδο 1 CSS</option>
+        <option value="css2">Επίπεδο 2 CSS</option>
+        <option selected="selected" value="css21">Επίπεδο 2.1 CSS</option>
+        <option value="css3">Επίπεδο 3 CSS</option>
+        <option value="svg">SVG</option>
+        <option value="svgbasic">Βασικό SVG</option>
+        <option value="svgtiny">μικρό SVG</option>  
+        <option value="mobile">Κινητό</option>
+        <option value="atsc-tv">Προφίλ ATSC TV</option>
+        <option value="tv">Προφίλ TV</option>
+      </select>
+    </td>
+    <th id="header_medium_uri">
+      <label for="medium_uri">Μέσο :</label>
+    </th>
+    <td headers="header_medium_uri">
+      <select id="medium_uri" name="usermedium">
+        <option selected="selected" value="all">Όλα</option>
+        <option value="aural">ακουστικό</option>
+        <option value="braille">σύστημα braille</option>
+        <option value="embossed">ανάγλυφο</option>
+        <option value="handheld">χειρός</option>
+        <option value="print">εκτύπωση</option>
+        <option value= "projection">προβολή</option>
+        <option value="screen">οθόνη</option>
+        <option value="tty">TTY</option>
+        <option value="tv">TV</option>
+        <option value="presentation">παρουσίαση</option>
+      </select>
+    </td>
+    </tr>
+    <tr>
+        <th id="header_warning_uri">
+      <label for="warning_uri">Προειδοποιήσεις :</label>
+    </th>
+    <td
+        colspan="3"
+         headers="header_warning_uri">
+      <select id="warning_uri" name="warning"> 
+        <option value="2">Όλα</option>
+        <option selected="selected" value="1">Κανονική αναφορά</option>
+        <option value="0">Πιο σημαντικό</option>
+        <option value="no">Καμία ειδοποίηση</option>
+      </select>
+    </td>
+    </tr>
+    </table>
+    </div><!-- item_contents -->
+  </fieldset><!-- invisible -->
+  
+  <p class="submit_button">
+  <input type="hidden" name="lang" value="el" />
+    <label title="Υποβολή URI για έλεγχο εγκυρότητας">
+      <input type="submit" value="Έλεγχος" />
+    </label>
+  </p>
+      </form>
+      </fieldset>
+
+      <fieldset id="validate-by-upload"  class="tabset_content front">
+        <legend class="tabset_label">Έλεγχος εγκυρότητας με μεταφορά αρχείου</legend>
+      <form method="post" enctype="multipart/form-data" action="validator">
+        <p class="instructions">Επιλέξτε το αρχείο που επιθυμείτε να ελέγξετε την εγκυρότητα :</p>
+        <p>
+          <label title="Επιλέξτε ένα Τοπικό Αρχείο για Μεταφορά και Έλεγχο Εγκυρότητας" for="file">Τοπικό αρχείο CSS :
+          <input type="file" id="file" name="file" size="30" /></label></p>
+                      <fieldset id="extra_opt_upload" class="moreoptions">
+    <legend class="toggletext" title="Show/Hide extra validation options">Περισσότερες επιλογές</legend>
+    <div class="options">
+    <table>
+    <tr>
+    <th id="header_profile_upload">
+      <label for="profile_upload">Προφίλ :</label>
+    </th>
+    <td headers="header_profile_upload">
+      <select id="profile_upload" name="profile">
+        <option value="none">Κανένα ειδικό προφίλ</option>
+        <option value="css1">Επίπεδο 1 CSS</option>
+        <option value="css2">Επίπεδο 2 CSS</option>
+        <option selected="selected" value="css21">Επίπεδο 2.1 CSS</option>
+        <option value="css3">Επίπεδο 3 CSS</option>
+        <option value="svg">SVG</option>
+        <option value="svgbasic">Βασικό SVG</option>
+        <option value="svgtiny">μικρό SVG</option>  
+        <option value="mobile">Κινητό</option>
+        <option value="atsc-tv">Προφίλ ATSC TV</option>
+        <option value="tv">Προφίλ TV</option>
+      </select>
+    </td>
+    <th id="header_medium_upload">
+      <label for="medium_upload">Μέσο :</label>
+    </th>
+    <td headers="header_medium_upload">
+      <select id="medium_upload" name="usermedium">
+        <option selected="selected" value="all">Όλα</option>
+        <option value="aural">ακουστικό</option>
+        <option value="braille">σύστημα braille</option>
+        <option value="embossed">ανάγλυφο</option>
+        <option value="handheld">χειρός</option>
+        <option value="print">εκτύπωση</option>
+        <option value= "projection">προβολή</option>
+        <option value="screen">οθόνη</option>
+        <option value="tty">TTY</option>
+        <option value="tv">TV</option>
+        <option value="presentation">παρουσίαση</option>
+      </select>
+    </td>
+    </tr>
+    <tr>
+        <th id="header_warning_upload">
+      <label for="warning_upload">Προειδοποιήσεις :</label>
+    </th>
+    <td
+        colspan="3"
+         headers="header_warning_upload">
+      <select id="warning_upload" name="warning"> 
+        <option value="2">Όλα</option>
+        <option selected="selected" value="1">Κανονική αναφορά</option>
+        <option value="0">Πιο σημαντικό</option>
+        <option value="no">Καμία ειδοποίηση</option>
+      </select>
+    </td>
+    </tr>
+    </table>
+    </div><!-- item_contents -->
+  </fieldset><!-- invisible -->
+  
+  <p class="submit_button">
+  <input type="hidden" name="lang" value="el" />
+    <label title="Υποβολή αρχείου για έλεγχο εγκυρότητας">
+      <input type="submit" value="Έλεγχος" />
+    </label>
+  </p>
+      </form>
+      </fieldset>
+
+      <fieldset id="validate-by-input"  class="tabset_content front">
+        <legend class="tabset_label">Έλεγχος εγκυρότητας με άμεση εισαγωγή</legend>
+        <form action="validator" enctype="multipart/form-data" method="post">
+        <p class="instructions">Εισάγετε το CSS που θέλετε να ελέγξετε την εγκυρότητα :</p>
+        <p>
+          <textarea name="text" rows="12" cols="70"></textarea>
+        </p>      
+              <fieldset id="extra_opt_input" class="moreoptions">
+    <legend class="toggletext" title="Show/Hide extra validation options">Περισσότερες επιλογές</legend>
+    <div class="options">
+    <table>
+    <tr>
+    <th id="header_profile_input">
+      <label for="profile_input">Προφίλ :</label>
+    </th>
+    <td headers="header_profile_input">
+      <select id="profile_input" name="profile">
+        <option value="none">Κανένα ειδικό προφίλ</option>
+        <option value="css1">Επίπεδο 1 CSS</option>
+        <option value="css2">Επίπεδο 2 CSS</option>
+        <option selected="selected" value="css21">Επίπεδο 2.1 CSS</option>
+        <option value="css3">Επίπεδο 3 CSS</option>
+        <option value="svg">SVG</option>
+        <option value="svgbasic">Βασικό SVG</option>
+        <option value="svgtiny">μικρό SVG</option>  
+        <option value="mobile">Κινητό</option>
+        <option value="atsc-tv">Προφίλ ATSC TV</option>
+        <option value="tv">Προφίλ TV</option>
+      </select>
+    </td>
+    <th id="header_medium_input">
+      <label for="medium_input">Μέσο :</label>
+    </th>
+    <td headers="header_medium_input">
+      <select id="medium_input" name="usermedium">
+        <option selected="selected" value="all">Όλα</option>
+        <option value="aural">ακουστικό</option>
+        <option value="braille">σύστημα braille</option>
+        <option value="embossed">ανάγλυφο</option>
+        <option value="handheld">χειρός</option>
+        <option value="print">εκτύπωση</option>
+        <option value= "projection">προβολή</option>
+        <option value="screen">οθόνη</option>
+        <option value="tty">TTY</option>
+        <option value="tv">TV</option>
+        <option value="presentation">παρουσίαση</option>
+      </select>
+    </td>
+    </tr>
+    <tr>
+        <th id="header_type_input">
+      <label for="type_input">Τύπος :</label>
+    </th>
+    <td headers="header_type_input">
+      <select id="type_input" name="type">
+        <option selected="selected" value="none">Αυτόματο</option>
+        <option value="html">HTML</option>
+        <option value="css">CSS</option>
+      </select>
+    </td>
+        <th id="header_warning_input">
+      <label for="warning_input">Προειδοποιήσεις :</label>
+    </th>
+    <td
+         headers="header_warning_input">
+      <select id="warning_input" name="warning"> 
+        <option value="2">Όλα</option>
+        <option selected="selected" value="1">Κανονική αναφορά</option>
+        <option value="0">Πιο σημαντικό</option>
+        <option value="no">Καμία ειδοποίηση</option>
+      </select>
+    </td>
+    </tr>
+    </table>
+    </div><!-- item_contents -->
+  </fieldset><!-- invisible -->
+  
+  <p class="submit_button">
+  <input type="hidden" name="lang" value="el" />
+    <label title="Εισάγετε αρχείο για έλεγχο εγκυρότητας">
+      <input type="submit" value="Έλεγχος" />
+    </label>
+  </p>
+      </form>
+      </fieldset>
+      </div><!-- fields -->
+  </div> <!-- frontforms -->
+  
+  <div class="intro" id="don_program"></div>  
+  <script type="text/javascript" src="http://www.w3.org/QA/Tools/don_prog.js"></script>
+  
+  
+  <div class="intro">
+  <p><strong>Σημείωση</strong>: Εάν επιθυμείτε να ελέγξετε την εγκυρότητα ενός CSS style sheet που είναι ενσωματωμένο σε ένα αρχείο (X)HTML, πρέπει αρχικά να <a href="http://validator.w3.org/">ελέγξετε ότι είναι έγκυρο το αρχείο (X)HTML που χρησιμοποιείτε</a>.
+  </p>
+  </div>
+  <ul class="navbar" id="menu">
+    <li><a href="about.html" title="Σχετικά με την υπηρεσία">Σχετικά</a> <span class="hideme">|</span></li>
+    <li><a href="documentation.html" title="Εγχειρίδια Χρήσης για την W3C CSS Υπηρεσία Ελέγχου Εγκυρότητας">Εγχειρίδια Χρήσης</a> <span class="hideme">|</span></li>
+    <li><a href="DOWNLOAD.html" title="Λήψη και Εγκατάσταση του Ελεγκτή Εγκυρότητας CSS">Λήψη Αρχείων</a> <span class="hideme">|</span></li>
+    <li><a href="Email.html" title="Πως να πείτε τη γνώμη σας γι' αυτή την υπηρεσία">Η Γνώμη Σας</a> <span class="hideme">|</span></li>
+    <li><a href="thanks.html" title="Ευχαριστίες">Ευχαριστίες</a></li>
+  </ul>
+
+   <ul id="lang_choice">
+        
+        <li><a href="validator.html.de"
+            lang="de"
+            xml:lang="de"
+            hreflang="de"
+            rel="alternate">Deutsch</a>
+        </li>
+        
+        <li><a href="validator.html.en"
+            lang="en"
+            xml:lang="en"
+            hreflang="en"
+            rel="alternate">English</a>
+        </li>
+        
+        <li><a href="validator.html.es"
+            lang="es"
+            xml:lang="es"
+            hreflang="es"
+            rel="alternate">Español</a>
+        </li>
+        
+        <li><a href="validator.html.fr"
+            lang="fr"
+            xml:lang="fr"
+            hreflang="fr"
+            rel="alternate">Français</a>
+        </li>
+        
+        <li><a href="validator.html.ko"
+            lang="ko"
+            xml:lang="ko"
+            hreflang="ko"
+            rel="alternate">한국어</a>
+        </li>
+        
+        <li><a href="validator.html.it"
+            lang="it"
+            xml:lang="it"
+            hreflang="it"
+            rel="alternate">Italiano</a>
+        </li>
+        
+        <li><a href="validator.html.nl"
+            lang="nl"
+            xml:lang="nl"
+            hreflang="nl"
+            rel="alternate">Nederlands</a>
+        </li>
+        
+        <li><a href="validator.html.ja"
+            lang="ja"
+            xml:lang="ja"
+            hreflang="ja"
+            rel="alternate">日本語</a>
+        </li>
+        
+        <li><a href="validator.html.pl-PL"
+            lang="pl-PL"
+            xml:lang="pl-PL"
+            hreflang="pl-PL"
+            rel="alternate">Polski</a>
+        </li>
+        
+        <li><a href="validator.html.pt-BR"
+            lang="pt-BR"
+            xml:lang="pt-BR"
+            hreflang="pt-BR"
+            rel="alternate">Português</a>
+        </li>
+        
+        <li><a href="validator.html.ru"
+            lang="ru"
+            xml:lang="ru"
+            hreflang="ru"
+            rel="alternate">Русский</a>
+        </li>
+        
+        <li><a href="validator.html.fa"
+            lang="fa"
+            xml:lang="fa"
+            hreflang="fa"
+            rel="alternate">فارسی</a>
+        </li>
+        
+        <li><a href="validator.html.sv"
+            lang="sv"
+            xml:lang="sv"
+            hreflang="sv"
+            rel="alternate">Svenska</a>
+        </li>
+        
+        <li><a href="validator.html.bg"
+            lang="bg"
+            xml:lang="bg"
+            hreflang="bg"
+            rel="alternate">Български</a>
+        </li>
+        
+        <li><a href="validator.html.uk"
+            lang="uk"
+            xml:lang="uk"
+            hreflang="uk"
+            rel="alternate">Українська</a>
+        </li>
+        
+        <li><a href="validator.html.cs"
+            lang="cs"
+            xml:lang="cs"
+            hreflang="cs"
+            rel="alternate">Čeština</a>
+        </li>
+        
+        <li><a href="validator.html.ro"
+            lang="ro"
+            xml:lang="ro"
+            hreflang="ro"
+            rel="alternate">Romanian</a>
+        </li>
+        
+        <li><a href="validator.html.hu"
+            lang="hu"
+            xml:lang="hu"
+            hreflang="hu"
+            rel="alternate">Magyar</a>
+        </li>
+        
+        <li><a href="validator.html.el"
+            lang="el"
+            xml:lang="el"
+            hreflang="el"
+            rel="alternate">Ελληνικά</a>
+        </li>
+        
+        <li><a href="validator.html.zh-cn"
+            lang="zh-cn"
+            xml:lang="zh-cn"
+            hreflang="zh-cn"
+            rel="alternate">简体中文</a>
+        </li>
+   </ul>
+
+<div id="footer">
+   <p id="activity_logos">
+     <a href="http://www.w3.org/QA/" title="Η Δραστηριότητα Διασφάλισης Ποιότητας της W3C, σας παρέχει δωρεάν ποιοτικά εργαλεία στο διαδίκτυο και περισσότερα"><img src="http://www.w3.org/QA/2002/12/qa-small.png" alt="QA" /></a><a href="http://www.w3.org/Style/CSS/learning" title="Μάθετε περισσότερα για τα Cascading Style Sheets"><img src="images/woolly-icon" alt="CSS" /></a>
+   </p>
+   <p id="support_logo">
+   <a href="http://www.w3.org/QA/Tools/Donate">
+   <img src="http://www.w3.org/QA/Tools/I_heart_validator" alt="I heart Validator logo" title=" Validators Donation Program" />
+   </a>
+   </p>
+
+    <p class="copyright">
+      <a rel="Copyright" href="http://www.w3.org/Consortium/Legal/ipr-notice#Copyright">Copyright</a> &copy; 1994-2009
+      <a href="http://www.w3.org/"><acronym title="World Wide Web Consortium">W3C</acronym></a>&reg;
+
+      (<a href="http://www.csail.mit.edu/"><acronym title="Massachusetts Institute of Technology">MIT</acronym></a>,
+      <a href="http://www.ercim.eu/"><acronym title="European Research Consortium for Informatics and Mathematics">ERCIM</acronym></a>,
+      <a href="http://www.keio.ac.jp/">Keio</a>),
+      All Rights Reserved.
+      W3C <a href="http://www.w3.org/Consortium/Legal/ipr-notice#Legal_Disclaimer">liability</a>,
+      <a href="http://www.w3.org/Consortium/Legal/ipr-notice#W3C_Trademarks">trademark</a>,
+      <a rel="Copyright" href="http://www.w3.org/Consortium/Legal/copyright-documents">document use</a>
+      and <a rel="Copyright" href="http://www.w3.org/Consortium/Legal/copyright-software">software licensing</a>
+
+      rules apply. Your interactions with this site are in accordance
+      with our <a href="http://www.w3.org/Consortium/Legal/privacy-statement#Public">public</a> and
+      <a href="http://www.w3.org/Consortium/Legal/privacy-statement#Members">Member</a> privacy
+      statements.
+    </p>
+</div>
+  </body>
+</html>
