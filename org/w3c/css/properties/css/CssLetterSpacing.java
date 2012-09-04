6a0dbc504241477df9d951dbb1b0f588608911a6
Yves Lafon <ylafon@w3.org>
2012-09-04 08:36:29 +0000

    use the root class CssValue

--- org/w3c/css/properties/css/CssLetterSpacing.java
+++ org/w3c/css/properties/css/CssLetterSpacing.java
@@ -16,93 +16,95 @@ import org.w3c.css.values.CssValue;
  */
 public class CssLetterSpacing extends CssProperty {
 
-    CssValue value;
+	public CssValue value;
 
-    /**
-     * Create a new CssLetterSpacing.
-     */
-    public CssLetterSpacing() {
-    }
+	/**
+	 * Create a new CssLetterSpacing.
+	 */
+	public CssLetterSpacing() {
+	}
 
-    /**
-     * Create a new CssLetterSpacing with an expression
-     *
-     * @param expression The expression
-     * @throws org.w3c.css.util.InvalidParamException
-     *          The expression is incorrect
-     */
-    public CssLetterSpacing(ApplContext ac, CssExpression expression,
-                            boolean check) throws InvalidParamException {
+	/**
+	 * Create a new CssLetterSpacing with an expression
+	 *
+	 * @param expression The expression
+	 * @throws org.w3c.css.util.InvalidParamException
+	 *          The expression is incorrect
+	 */
+	public CssLetterSpacing(ApplContext ac, CssExpression expression,
+							boolean check) throws InvalidParamException {
 
-        throw new InvalidParamException("unrecognize", ac);
-    }
+		throw new InvalidParamException("unrecognize", ac);
+	}
 
-    public CssLetterSpacing(ApplContext ac, CssExpression expression)
-            throws InvalidParamException {
-        this(ac, expression, false);
-    }
+	public CssLetterSpacing(ApplContext ac, CssExpression expression)
+			throws InvalidParamException {
+		this(ac, expression, false);
+	}
 
-    /**
-     * Returns the value of this property
-     */
-    public Object get() {
-        return value;
-    }
+	/**
+	 * Returns the value of this property
+	 */
+	public Object get() {
+		return value;
+	}
 
-    /**
-     * Returns the name of this property
-     */
-    public final String getPropertyName() {
-        return "letter-spacing";
-    }
+	/**
+	 * Returns the name of this property
+	 */
+	public final String getPropertyName() {
+		return "letter-spacing";
+	}
 
-    /**
-     * Returns true if this property is "softly" inherited
-     * e.g. his value equals inherit
-     */
-    public boolean isSoftlyInherited() {
-        return value == inherit;
-    }
+	/**
+	 * Returns true if this property is "softly" inherited
+	 * e.g. his value equals inherit
+	 */
+	public boolean isSoftlyInherited() {
+		return value == inherit;
+	}
 
-    /**
-     * Returns a string representation of the object.
-     */
-    public String toString() {
-        return value.toString();
-    }
+	/**
+	 * Returns a string representation of the object.
+	 */
+	public String toString() {
+		return value.toString();
+	}
 
-    /**
-     * Adds this property to a style.
-     *
-     * @param style The style.
-     */
-    public void addToStyle(ApplContext ac, CssStyle style) {
-        Css1Style style0 = (Css1Style) style;
-        if (style0.cssLetterSpacing != null)
-            style0.addRedefinitionWarning(ac, this);
-        style0.cssLetterSpacing = this;
-    }
+	/**
+	 * Adds this property to a style.
+	 *
+	 * @param style The style.
+	 */
+	public void addToStyle(ApplContext ac, CssStyle style) {
+		Css1Style style0 = (Css1Style) style;
+		if (style0.cssLetterSpacing != null) {
+			style0.addRedefinitionWarning(ac, this);
+		}
+		style0.cssLetterSpacing = this;
+	}
 
-    /**
-     * Get this property in the style.
-     *
-     * @param style   The style where the property is
-     * @param resolve if true, resolve the style to find this property
-     */
-    public CssProperty getPropertyInStyle(CssStyle style, boolean resolve) {
-        if (resolve) {
-            return ((Css1Style) style).getLetterSpacing();
-        } else {
-            return ((Css1Style) style).cssLetterSpacing;
-        }
-    }
+	/**
+	 * Get this property in the style.
+	 *
+	 * @param style   The style where the property is
+	 * @param resolve if true, resolve the style to find this property
+	 */
+	public CssProperty getPropertyInStyle(CssStyle style, boolean resolve) {
+		if (resolve) {
+			return ((Css1Style) style).getLetterSpacing();
+		} else {
+			return ((Css1Style) style).cssLetterSpacing;
+		}
+	}
 
-    /**
-     * Compares two properties for equality.
-     *
-     * @param property The other property.
-     */
-    public boolean equals(CssProperty property) {
-        return false;
-    }
+	/**
+	 * Compares two properties for equality.
+	 *
+	 * @param property The other property.
+	 */
+	public boolean equals(CssProperty property) {
+		return (property instanceof CssLetterSpacing) &&
+				((CssLetterSpacing) property).value.equals(value);
+	}
 }
--- org/w3c/css/properties/css1/CssLetterSpacing.java
+++ org/w3c/css/properties/css1/CssLetterSpacing.java
@@ -5,7 +5,6 @@
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.properties.css1;
 
-import org.w3c.css.properties.css.CssProperty;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 import org.w3c.css.values.CssExpression;
@@ -15,90 +14,56 @@ import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
 
 /**
- * @spec  http://www.w3.org/TR/2008/REC-CSS1-20080411/#letter-spacing
- * @version $Revision$
+ * @spec http://www.w3.org/TR/2008/REC-CSS1-20080411/#letter-spacing
  */
 public class CssLetterSpacing extends org.w3c.css.properties.css.CssLetterSpacing {
 
-    private CssValue value;
-    private static CssIdent normal = CssIdent.getIdent("normal");
+	private static CssIdent normal = CssIdent.getIdent("normal");
 
-    /**
-     * Create a new CssLetterSpacing.
-     */
-    public CssLetterSpacing() {
-        value = normal;
-    }
+	/**
+	 * Create a new CssLetterSpacing.
+	 */
+	public CssLetterSpacing() {
+		value = normal;
+	}
 
-    /**
-     * Create a new CssLetterSpacing with an expression
-     *
-     * @param expression The expression
-     * @throws InvalidParamException The expression is incorrect
-     */
-    public CssLetterSpacing(ApplContext ac, CssExpression expression,
-                          boolean check) throws InvalidParamException {
+	/**
+	 * Create a new CssLetterSpacing with an expression
+	 *
+	 * @param expression The expression
+	 * @throws InvalidParamException The expression is incorrect
+	 */
+	public CssLetterSpacing(ApplContext ac, CssExpression expression,
+							boolean check) throws InvalidParamException {
 
-        if (check && expression.getCount() > 1) {
-            throw new InvalidParamException("unrecognize", ac);
-        }
+		if (check && expression.getCount() > 1) {
+			throw new InvalidParamException("unrecognize", ac);
+		}
 
-        CssValue val = expression.getValue();
+		CssValue val = expression.getValue();
 
-        setByUser();
+		setByUser();
 
-        switch (val.getType()) {
-            case CssTypes.CSS_NUMBER:
-                val = ((CssNumber) val).getLength();
-            case CssTypes.CSS_LENGTH:
-                value = val;
-                break;
-            case CssTypes.CSS_IDENT:
-                if (inherit.equals(val) || normal.equals(val)) {
-                    value = val;
-                    break;
-                }
-            default:
-                throw new InvalidParamException("value", expression.getValue(),
-                        getPropertyName(), ac);
-        }
-        expression.next();
-    }
+		switch (val.getType()) {
+			case CssTypes.CSS_NUMBER:
+				val = ((CssNumber) val).getLength();
+			case CssTypes.CSS_LENGTH:
+				value = val;
+				break;
+			case CssTypes.CSS_IDENT:
+				if (normal.equals(val)) {
+					value = val;
+					break;
+				}
+			default:
+				throw new InvalidParamException("value", expression.getValue(),
+						getPropertyName(), ac);
+		}
+		expression.next();
+	}
 
-    public CssLetterSpacing(ApplContext ac, CssExpression expression)
-            throws InvalidParamException {
-        this(ac, expression, false);
-    }
-
-    /**
-     * Returns the value of this property
-     */
-    public Object get() {
-        return value;
-    }
-
-    /**
-     * Returns true if this property is "softly" inherited
-     * e.g. his value equals inherit
-     */
-    public boolean isSoftlyInherited() {
-        return value == inherit;
-    }
-
-    /**
-     * Returns a string representation of the object.
-     */
-    public String toString() {
-        return value.toString();
-    }
-
-    /**
-     * Compares two properties for equality.
-     *
-     * @param property The other property.
-     */
-    public boolean equals(CssProperty property) {
-        return (property instanceof CssLetterSpacing &&
-                value.equals(((CssLetterSpacing) property).value));
-    }
+	public CssLetterSpacing(ApplContext ac, CssExpression expression)
+			throws InvalidParamException {
+		this(ac, expression, false);
+	}
 }
