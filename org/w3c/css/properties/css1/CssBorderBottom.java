21750e1474d9a57f49b3380439a6a3aaf3bd3694
Yves Lafon <ylafon@w3.org>
2012-09-28 08:32:00 +0000

    NPE issue

--- org/w3c/css/properties/css1/CssBorderBottom.java
+++ org/w3c/css/properties/css1/CssBorderBottom.java
@@ -19,52 +19,52 @@ import org.w3c.css.values.CssExpression;
  */
 public class CssBorderBottom extends org.w3c.css.properties.css.CssBorderBottom {
 
-    /**
-     * Create a new CssBorderBottom
-     */
-    public CssBorderBottom() {
-    }
+	/**
+	 * Create a new CssBorderBottom
+	 */
+	public CssBorderBottom() {
+	}
 
-    /**
-     * Set the value of the property<br/>
-     * Does not check the number of values
-     *
-     * @param expression The expression for this property
-     * @throws org.w3c.css.util.InvalidParamException
-     *          The expression is incorrect
-     */
-    public CssBorderBottom(ApplContext ac, CssExpression expression)
-            throws InvalidParamException {
-        this(ac, expression, false);
-    }
+	/**
+	 * Set the value of the property<br/>
+	 * Does not check the number of values
+	 *
+	 * @param expression The expression for this property
+	 * @throws org.w3c.css.util.InvalidParamException
+	 *          The expression is incorrect
+	 */
+	public CssBorderBottom(ApplContext ac, CssExpression expression)
+			throws InvalidParamException {
+		this(ac, expression, false);
+	}
 
-    /**
-     * Set the value of the property
-     *
-     * @param expression The expression for this property
-     * @param check      set it to true to check the number of values
-     * @throws org.w3c.css.util.InvalidParamException
-     *          The expression is incorrect
-     */
-    public CssBorderBottom(ApplContext ac, CssExpression expression,
-                           boolean check) throws InvalidParamException {
-        CssBorder.SideValues values = CssBorder.checkBorderSide(ac, this, expression, check);
-        if (values.width != null) {
-            _width = new CssBorderBottomWidth();
-            _width.setByUser();
-            _width.value = values.width;
-        }
-        if (values.style != null) {
-            _style = new CssBorderBottomStyle();
-            _style.setByUser();
-            _style.value = values.style;
-        }
-        if (values.color != null) {
-            _color = new CssBorderBottomColor();
-            _color.setByUser();
-            _color.value = values.color;
-        }
-    }
+	/**
+	 * Set the value of the property
+	 *
+	 * @param expression The expression for this property
+	 * @param check      set it to true to check the number of values
+	 * @throws org.w3c.css.util.InvalidParamException
+	 *          The expression is incorrect
+	 */
+	public CssBorderBottom(ApplContext ac, CssExpression expression,
+						   boolean check) throws InvalidParamException {
+		CssBorder.SideValues values = CssBorder.checkBorderSide(ac, this, expression, check);
+		if (values.width != null) {
+			_width = new CssBorderBottomWidth();
+			_width.setByUser();
+			_width.value = values.width;
+		}
+		if (values.style != null) {
+			_style = new CssBorderBottomStyle();
+			_style.setByUser();
+			_style.value = values.style;
+		}
+		if (values.color != null) {
+			_color = new CssBorderBottomColor();
+			_color.setByUser();
+			_color.value = values.color;
+		}
+	}
 
 	/**
 	 * Add this property to the CssStyle
@@ -81,43 +81,48 @@ public class CssBorderBottom extends org.w3c.css.properties.css.CssBorderBottom 
 		if (_width != null) {
 			_width.addToStyle(ac, style);
 		}
-		if (css1Style.cssBorder.borderColor.bottom != null) {
-			style.addRedefinitionWarning(ac, _color);
+		if (_color != null) {
+			if (css1Style.cssBorder.borderColor.bottom != null) {
+				style.addRedefinitionWarning(ac, _color);
+			}
+			css1Style.cssBorder.borderColor.bottom = _color;
 		}
-		css1Style.cssBorder.borderColor.bottom = _color;
-		if (css1Style.cssBorder.borderStyle.bottom != null) {
-			style.addRedefinitionWarning(ac, _style);
+		if (_style != null) {
+			if (css1Style.cssBorder.borderStyle.bottom != null) {
+				style.addRedefinitionWarning(ac, _style);
+			}
+			css1Style.cssBorder.borderStyle.bottom = _style;
 		}
-		css1Style.cssBorder.borderStyle.bottom = _style;
 	}
-    /**
-     * Returns a string representation of the object.
-     */
-    public String toString() {
-        if (_width != null && inherit == _width.value) {
-            return inherit.toString();
-        }
-        StringBuilder sb = new StringBuilder();
-        boolean first = true;
-        if (_width != null) {
-            sb.append(_width);
-            first = false;
-        }
-        if (_style != null) {
-            if (first) {
-                sb.append(_style);
-            } else {
-                sb.append(' ').append(_style);
-            }
-            first = false;
-        }
-        if (_color != null) {
-            if (first) {
-                sb.append(_color);
-            } else {
-                sb.append(' ').append(_color);
-            }
-        }
-        return sb.toString();
-    }
+
+	/**
+	 * Returns a string representation of the object.
+	 */
+	public String toString() {
+		if (_width != null && inherit == _width.value) {
+			return inherit.toString();
+		}
+		StringBuilder sb = new StringBuilder();
+		boolean first = true;
+		if (_width != null) {
+			sb.append(_width);
+			first = false;
+		}
+		if (_style != null) {
+			if (first) {
+				sb.append(_style);
+			} else {
+				sb.append(' ').append(_style);
+			}
+			first = false;
+		}
+		if (_color != null) {
+			if (first) {
+				sb.append(_color);
+			} else {
+				sb.append(' ').append(_color);
+			}
+		}
+		return sb.toString();
+	}
 }
--- org/w3c/css/properties/css1/CssBorderRight.java
+++ org/w3c/css/properties/css1/CssBorderRight.java
@@ -19,50 +19,52 @@ import org.w3c.css.values.CssExpression;
  */
 public class CssBorderRight extends org.w3c.css.properties.css.CssBorderRight {
 
-    /**
-     * Create a new CssBorderRight
-     */
-    public CssBorderRight() {
-    }
+	/**
+	 * Create a new CssBorderRight
+	 */
+	public CssBorderRight() {
+	}
 
-    /**
-     * Set the value of the property<br/>
-     * Does not check the number of values
-     *
-     * @param expression The expression for this property
-     * @throws org.w3c.css.util.InvalidParamException The expression is incorrect
-     */
-    public CssBorderRight(ApplContext ac, CssExpression expression)
-            throws InvalidParamException {
-        this(ac, expression, false);
-    }
+	/**
+	 * Set the value of the property<br/>
+	 * Does not check the number of values
+	 *
+	 * @param expression The expression for this property
+	 * @throws org.w3c.css.util.InvalidParamException
+	 *          The expression is incorrect
+	 */
+	public CssBorderRight(ApplContext ac, CssExpression expression)
+			throws InvalidParamException {
+		this(ac, expression, false);
+	}
 
-    /**
-     * Set the value of the property
-     *
-     * @param expression The expression for this property
-     * @param check      set it to true to check the number of values
-     * @throws org.w3c.css.util.InvalidParamException The expression is incorrect
-     */
-    public CssBorderRight(ApplContext ac, CssExpression expression,
-                          boolean check) throws InvalidParamException {
-        CssBorder.SideValues values = CssBorder.checkBorderSide(ac, this, expression, check);
-        if (values.width != null) {
-            _width = new CssBorderRightWidth();
-            _width.setByUser();
-            _width.value = values.width;
-        }
-        if (values.style != null) {
-            _style = new CssBorderRightStyle();
-            _style.setByUser();
-            _style.value = values.style;
-        }
-        if (values.color != null) {
-            _color = new CssBorderRightColor();
-            _color.setByUser();
-            _color.value = values.color;
-        }
-    }
+	/**
+	 * Set the value of the property
+	 *
+	 * @param expression The expression for this property
+	 * @param check      set it to true to check the number of values
+	 * @throws org.w3c.css.util.InvalidParamException
+	 *          The expression is incorrect
+	 */
+	public CssBorderRight(ApplContext ac, CssExpression expression,
+						  boolean check) throws InvalidParamException {
+		CssBorder.SideValues values = CssBorder.checkBorderSide(ac, this, expression, check);
+		if (values.width != null) {
+			_width = new CssBorderRightWidth();
+			_width.setByUser();
+			_width.value = values.width;
+		}
+		if (values.style != null) {
+			_style = new CssBorderRightStyle();
+			_style.setByUser();
+			_style.value = values.style;
+		}
+		if (values.color != null) {
+			_color = new CssBorderRightColor();
+			_color.setByUser();
+			_color.value = values.color;
+		}
+	}
 
 	/**
 	 * Add this property to the CssStyle
@@ -79,45 +81,50 @@ public class CssBorderRight extends org.w3c.css.properties.css.CssBorderRight {
 		if (_width != null) {
 			_width.addToStyle(ac, style);
 		}
-		if (css1Style.cssBorder.borderColor.right != null) {
-			style.addRedefinitionWarning(ac, _color);
+		if (_color != null) {
+			if (css1Style.cssBorder.borderColor.right != null) {
+				style.addRedefinitionWarning(ac, _color);
+				css1Style.cssBorder.borderColor.right = _color;
+			}
 		}
-		css1Style.cssBorder.borderColor.right = _color;
-		if (css1Style.cssBorder.borderStyle.right != null) {
-			style.addRedefinitionWarning(ac, _style);
+		if (_style != null) {
+			if (css1Style.cssBorder.borderStyle.right != null) {
+				style.addRedefinitionWarning(ac, _style);
+			}
+			css1Style.cssBorder.borderStyle.right = _style;
 		}
-		css1Style.cssBorder.borderStyle.right = _style;
 	}
 
 	/**
-     * Returns a string representation of the object.
-     */
-    public String toString() {
-        if (_width != null && inherit == _width.value) {
-            return inherit.toString();
-        }
-        StringBuilder sb = new StringBuilder();
-        boolean first = true;
-        if (_width != null) {
-            sb.append(_width);
-            first = false;
-        }
-        if (_style != null) {
-            if (first) {
-                sb.append(_style);
-            } else {
-                sb.append(' ').append(_style);
-            }
-            first = false;
-        }
-        if (_color != null) {
-            if (first) {
-                sb.append(_color);
-            } else {
-                sb.append(' ').append(_color);
-            }
-        }
-        return sb.toString();
-    }
+	 * Returns a string representation of the object.
+	 */
+
+	public String toString() {
+		if (_width != null && inherit == _width.value) {
+			return inherit.toString();
+		}
+		StringBuilder sb = new StringBuilder();
+		boolean first = true;
+		if (_width != null) {
+			sb.append(_width);
+			first = false;
+		}
+		if (_style != null) {
+			if (first) {
+				sb.append(_style);
+			} else {
+				sb.append(' ').append(_style);
+			}
+			first = false;
+		}
+		if (_color != null) {
+			if (first) {
+				sb.append(_color);
+			} else {
+				sb.append(' ').append(_color);
+			}
+		}
+		return sb.toString();
+	}
 
 }
