1d8851cd26f31ce7e5016075f965745a6a56a042
Yves Lafon <ylafon@w3.org>
2013-02-19 13:13:29 +0000

    negative duration is now invalid (ie: not considered as being 0)

--- org/w3c/css/properties/css3/CssAnimationDuration.java
+++ org/w3c/css/properties/css3/CssAnimationDuration.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import static org.w3c.css.values.CssOperator.COMMA;
 
 /**
- * @spec http://www.w3.org/TR/2012/WD-css3-animations-20120403/#animation-duration
+ * @spec http://www.w3.org/TR/2013/WD-css3-animations-20130218/#animation-duration
  */
 public class CssAnimationDuration extends org.w3c.css.properties.css.CssAnimationDuration {
 
@@ -51,7 +51,7 @@ public class CssAnimationDuration extends org.w3c.css.properties.css.CssAnimatio
 			switch (val.getType()) {
 				case CssTypes.CSS_TIME:
 					CssTime t = val.getTime();
-					t.warnPositiveness(ac, this);
+					t.checkPositiveness(ac, this);
 					values.add(val);
 					break;
 				case CssTypes.CSS_IDENT:
