15165c86feecd8436c385c17dd0e711d2daceed7
Yves Lafon <ylafon@w3.org>
2014-11-24 15:49:06 +0000

    link to new draft

--- org/w3c/css/properties/css3/CssAlignContent.java
+++ org/w3c/css/properties/css3/CssAlignContent.java
@@ -13,7 +13,7 @@ import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
 
 /**
- * @spec http://www.w3.org/TR/2012/CR-css3-flexbox-20120918/#align-content
+ * @spec http://www.w3.org/TR/2014/WD-css-flexbox-1-20140925/#propdef-align-content
  */
 public class CssAlignContent extends org.w3c.css.properties.css.CssAlignContent {
 
