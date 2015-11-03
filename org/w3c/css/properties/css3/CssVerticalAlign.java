ff0dca8be71ed3aed9b11634094a28eac14f960a
Yves Lafon <ylafon@w3.org>
2015-11-03 17:22:33 +0100

    updated vertical-align per http://www.w3.org/TR/2015/WD-css-inline-3-20150917/#propdef-vertical-align

--- org/w3c/css/properties/css3/CssVerticalAlign.java
+++ org/w3c/css/properties/css3/CssVerticalAlign.java
@@ -1,7 +1,7 @@
-// $Id$
+//
 // Author: Yves Lafon <ylafon@w3.org>
 //
-// (c) COPYRIGHT MIT, ERCIM and Keio University, 2012.
+// (c) COPYRIGHT MIT, ERCIM, Keio, Beihang 2015.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.properties.css3;
 
@@ -9,54 +9,23 @@ import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 import org.w3c.css.values.CssExpression;
 import org.w3c.css.values.CssIdent;
+import org.w3c.css.values.CssOperator;
 import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
+import org.w3c.css.values.CssValueList;
+
+import java.util.ArrayList;
 
 /**
- * @spec http://www.w3.org/TR/2002/WD-css3-linebox-20020515/#vertical-align
+ * @spec http://www.w3.org/TR/2015/WD-css-inline-3-20150917/#propdef-vertical-align
  */
 public class CssVerticalAlign extends org.w3c.css.properties.css.CssVerticalAlign {
-
-	private static CssIdent[] allowed_values;
-
-	static {
-		String id_values[] = {"auto", "baseline", "sub", "super", "top", "text-top",
-				"central", "middle", "bottom", "text-bottom", "use-script"};
-
-		allowed_values = new CssIdent[id_values.length];
-		int i = 0;
-		for (String s : id_values) {
-			allowed_values[i++] = CssIdent.getIdent(s);
-		}
-	}
-
-	public static CssIdent getMatchingIdent(CssIdent ident) {
-		for (CssIdent id : allowed_values) {
-			if (id.equals(ident)) {
-				return id;
-			}
-		}
-		return null;
-	}
 
 	/**
 	 * Create a new CssVerticalAlign
 	 */
 	public CssVerticalAlign() {
 		value = initial;
-	}
-
-	/**
-	 * Set the value of the property<br/>
-	 * Does not check the number of values
-	 *
-	 * @param expression The expression for this property
-	 * @throws org.w3c.css.util.InvalidParamException
-	 *          The expression is incorrect
-	 */
-	public CssVerticalAlign(ApplContext ac, CssExpression expression)
-			throws InvalidParamException {
-		this(ac, expression, false);
 	}
 
 	/**
@@ -69,37 +38,83 @@ public class CssVerticalAlign extends org.w3c.css.properties.css.CssVerticalAlig
 	 */
 	public CssVerticalAlign(ApplContext ac, CssExpression expression,
 							boolean check) throws InvalidParamException {
-		setByUser();
-		CssValue val = expression.getValue();
 
-		if (check && expression.getCount() > 1) {
+		if (check && expression.getCount() > 2) {
 			throw new InvalidParamException("unrecognize", ac);
 		}
 
-		switch (val.getType()) {
-			case CssTypes.CSS_NUMBER:
-				val = val.getLength();
-			case CssTypes.CSS_LENGTH:
-				value = val;
-				break;
-			case CssTypes.CSS_PERCENTAGE:
-				value = val;
-				break;
-			case CssTypes.CSS_IDENT:
-				if (inherit.equals(val)) {
-					value = inherit;
+		ArrayList<CssValue> v = new ArrayList<>(2);
+		setByUser();
+		char op;
+		CssValue val;
+		CssIdent res;
+		boolean got_shift = false;
+		boolean got_alignment = false;
+
+		while (!expression.end()) {
+			val = expression.getValue();
+			op = expression.getOperator();
+
+			switch (val.getType()) {
+				case CssTypes.CSS_NUMBER:
+					val = val.getLength();
+				case CssTypes.CSS_LENGTH:
+				case CssTypes.CSS_PERCENTAGE:
+					if (got_shift) {
+						throw new InvalidParamException("value", expression.getValue(),
+								getPropertyName(), ac);
+					}
+					got_shift = true;
+					v.add(val);
 					break;
-				}
-				val = getMatchingIdent((CssIdent) val);
-				if (val != null) {
-					value = val;
-					break;
-				}
-			default:
-				throw new InvalidParamException("value", val,
-						getPropertyName(), ac);
+				case CssTypes.CSS_IDENT:
+					CssIdent id = (CssIdent) val;
+					if (inherit.equals(id)) {
+						// inherit can only be alone
+						if (expression.getCount() > 1) {
+							throw new InvalidParamException("value", expression.getValue(),
+									getPropertyName(), ac);
+						}
+						value = inherit;
+						break;
+					}
+					if (!got_shift) {
+						res = CssBaselineShift.getAllowedIdent(id);
+						if (res != null) {
+							v.add(res);
+							got_shift = true;
+							break;
+						}
+					}
+					if (!got_alignment) {
+						res = CssAlignmentBaseline.getAllowedIdent(id);
+						if (res != null) {
+							got_alignment = true;
+							v.add(res);
+							break;
+						}
+					}
+					// failed...
+				default:
+					throw new InvalidParamException("value", val,
+							getPropertyName(), ac);
+			}
+			if (op != CssOperator.SPACE) {
+				throw new InvalidParamException("operator",
+						((new Character(op)).toString()), ac);
+			}
+			expression.next();
 		}
-		expression.next();
+		if (value != inherit) {
+			value = (v.size() == 1) ? v.get(0) : new CssValueList(v);
+		}
 	}
+
+	public CssVerticalAlign(ApplContext ac, CssExpression expression)
+			throws InvalidParamException {
+		this(ac, expression, false);
+	}
+
+
 }
 
