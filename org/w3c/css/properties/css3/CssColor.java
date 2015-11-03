9643834235e96b085baae2e508b6d818fe136a08
Yves Lafon <ylafon@w3.org>
2015-11-03 13:36:43 +0100

    removed $Revision$ in comment

--- org/w3c/css/properties/css3/CssColor.java
+++ org/w3c/css/properties/css3/CssColor.java
@@ -15,150 +15,149 @@ import org.w3c.css.values.CssValue;
 
 /**
  * @spec http://www.w3.org/TR/2011/REC-css3-color-20110607/#color0
- * @version $Revision$
  */
 public class CssColor extends org.w3c.css.properties.css.CssColor {
 
-    CssValue color;
-    String attrvalue = null;
+	CssValue color;
+	String attrvalue = null;
 
-    /**
-     * Create a new CssColor
-     */
-    public CssColor() {
-        color = inherit;
-    }
+	/**
+	 * Create a new CssColor
+	 */
+	public CssColor() {
+		color = inherit;
+	}
 
-    /**
-     * Set the value of the property
-     *
-     * @param expression The expression for this property
-     * @throws org.w3c.css.util.InvalidParamException
-     *          Values are incorrect
-     */
-    public CssColor(ApplContext ac, CssExpression expression, boolean check)
-            throws InvalidParamException {
+	/**
+	 * Set the value of the property
+	 *
+	 * @param expression The expression for this property
+	 * @throws org.w3c.css.util.InvalidParamException
+	 *          Values are incorrect
+	 */
+	public CssColor(ApplContext ac, CssExpression expression, boolean check)
+			throws InvalidParamException {
 
-        if (check && expression.getCount() > 1) {
-            throw new InvalidParamException("unrecognize", ac);
-        }
+		if (check && expression.getCount() > 1) {
+			throw new InvalidParamException("unrecognize", ac);
+		}
 
-        org.w3c.css.values.CssColor tempcolor = new org.w3c.css.values.CssColor();
+		org.w3c.css.values.CssColor tempcolor = new org.w3c.css.values.CssColor();
 
-        CssValue val = expression.getValue();
-        setByUser();
+		CssValue val = expression.getValue();
+		setByUser();
 
-        switch (val.getType()) {
+		switch (val.getType()) {
 			case CssTypes.CSS_HASH_IDENT:
 				org.w3c.css.values.CssColor c = new org.w3c.css.values.CssColor();
 				c.setShortRGBColor(val.toString(), ac);
 				color = c;
 				break;
-            case CssTypes.CSS_IDENT:
-                if (inherit.equals(val)) {
-                    color = inherit;
-                } else if (currentColor.equals(val)) {
-                    color = currentColor;
-                } else {
-                    color = new org.w3c.css.values.CssColor(ac, (String) val.get());
-                }
-                break;
-            case CssTypes.CSS_COLOR:
-                color = val;
-                break;
-            case CssTypes.CSS_FUNCTION:
-                CssFunction attr = (CssFunction) val;
-                CssExpression params = attr.getParameters();
-                String fname = attr.getName();
+			case CssTypes.CSS_IDENT:
+				if (inherit.equals(val)) {
+					color = inherit;
+				} else if (currentColor.equals(val)) {
+					color = currentColor;
+				} else {
+					color = new org.w3c.css.values.CssColor(ac, (String) val.get());
+				}
+				break;
+			case CssTypes.CSS_COLOR:
+				color = val;
+				break;
+			case CssTypes.CSS_FUNCTION:
+				CssFunction attr = (CssFunction) val;
+				CssExpression params = attr.getParameters();
+				String fname = attr.getName();
 
-                if (fname.equals("attr")) {
-                    CssValue v1 = params.getValue();
-                    params.next();
-                    CssValue v2 = params.getValue();
-                    if ((params.getCount() != 2)) {
-                        throw new InvalidParamException("value",
-                                params.getValue(),
-                                getPropertyName(), ac);
-                    } else if (v1.getType() != CssTypes.CSS_IDENT) {
-                        throw new InvalidParamException("value",
-                                params.getValue(),
-                                getPropertyName(), ac);
+				if (fname.equals("attr")) {
+					CssValue v1 = params.getValue();
+					params.next();
+					CssValue v2 = params.getValue();
+					if ((params.getCount() != 2)) {
+						throw new InvalidParamException("value",
+								params.getValue(),
+								getPropertyName(), ac);
+					} else if (v1.getType() != CssTypes.CSS_IDENT) {
+						throw new InvalidParamException("value",
+								params.getValue(),
+								getPropertyName(), ac);
 
-                    } else if (!(v2.toString().equals("color"))) {
-                        throw new InvalidParamException("value",
-                                params.getValue(),
-                                getPropertyName(), ac);
-                    } else {
-                        attrvalue = "attr(" + v1 + ", " + v2 + ')';
-                    }
-                } else {
-                    throw new InvalidParamException("value",
-                            params.getValue(),
-                            getPropertyName(), ac);
-                }
-                break;
-            default:
-                throw new InvalidParamException("value", expression.getValue(),
-                        getPropertyName(), ac);
-        }
-        expression.next();
-    }
+					} else if (!(v2.toString().equals("color"))) {
+						throw new InvalidParamException("value",
+								params.getValue(),
+								getPropertyName(), ac);
+					} else {
+						attrvalue = "attr(" + v1 + ", " + v2 + ')';
+					}
+				} else {
+					throw new InvalidParamException("value",
+							params.getValue(),
+							getPropertyName(), ac);
+				}
+				break;
+			default:
+				throw new InvalidParamException("value", expression.getValue(),
+						getPropertyName(), ac);
+		}
+		expression.next();
+	}
 
-    public CssColor(ApplContext ac, CssExpression expression)
-            throws InvalidParamException {
-        this(ac, expression, false);
-    }
+	public CssColor(ApplContext ac, CssExpression expression)
+			throws InvalidParamException {
+		this(ac, expression, false);
+	}
 
-    /**
-     * Returns the value of this property
-     */
-    public Object get() {
-        return color;
-    }
+	/**
+	 * Returns the value of this property
+	 */
+	public Object get() {
+		return color;
+	}
 
-    /**
-     * Returns the color
-     */
-    public org.w3c.css.values.CssColor getColor() {
-        if (inherit.equals(color) || currentColor.equals(color)) {
-            /*
-             System.err.println("[ERROR] org.w3c.css.properties.CssColor");
+	/**
+	 * Returns the color
+	 */
+	public org.w3c.css.values.CssColor getColor() {
+		if (inherit.equals(color) || currentColor.equals(color)) {
+			/*
+			 System.err.println("[ERROR] org.w3c.css.properties.CssColor");
              System.err.println("[ERROR] value is inherited");
            */
-            return null;
-        } else {
-            return (org.w3c.css.values.CssColor) color;
-        }
-    }
+			return null;
+		} else {
+			return (org.w3c.css.values.CssColor) color;
+		}
+	}
 
-    /**
-     * Returns true if this property is "softly" inherited
-     * e.g. his value equals inherit
-     */
-    public boolean isSoftlyInherited() {
-        return inherit.equals(color) || currentColor.equals(color);
-    }
+	/**
+	 * Returns true if this property is "softly" inherited
+	 * e.g. his value equals inherit
+	 */
+	public boolean isSoftlyInherited() {
+		return inherit.equals(color) || currentColor.equals(color);
+	}
 
-    /**
-     * Returns a string representation of the object.
-     */
-    public String toString() {
-        if (attrvalue != null) {
-            return attrvalue;
-        } else {
-            return color.toString();
-        }
-    }
+	/**
+	 * Returns a string representation of the object.
+	 */
+	public String toString() {
+		if (attrvalue != null) {
+			return attrvalue;
+		} else {
+			return color.toString();
+		}
+	}
 
-    /**
-     * Compares two properties for equality.
-     *
-     * @param property The other property.
-     */
-    public boolean equals(CssProperty property) {
-        return (property instanceof CssColor &&
-                color.equals(((CssColor) property).color));
-    }
+	/**
+	 * Compares two properties for equality.
+	 *
+	 * @param property The other property.
+	 */
+	public boolean equals(CssProperty property) {
+		return (property instanceof CssColor &&
+				color.equals(((CssColor) property).color));
+	}
 
 
 }
