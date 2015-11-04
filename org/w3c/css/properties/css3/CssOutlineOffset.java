ec556d1c549e1b340de5f6862db6cb847df22583
Yves Lafon <ylafon@w3.org>
2015-11-04 16:33:20 +0100

    conforms to http://www.w3.org/TR/2015/CR-css-ui-3-20150707/#propdef-outline-offset @spec updated

--- org/w3c/css/properties/css3/CssOutlineOffset.java
+++ org/w3c/css/properties/css3/CssOutlineOffset.java
@@ -1,7 +1,7 @@
-// $Id$
+//
 // Author: Yves Lafon <ylafon@w3.org>
 //
-// (c) COPYRIGHT MIT, ERCIM and Keio University, 2012.
+// (c) COPYRIGHT MIT, ERCIM, Keio, Beihang, 2015.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.properties.css3;
 
@@ -12,7 +12,7 @@ import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
 
 /**
- * @spec http://www.w3.org/TR/2012/WD-css3-ui-20120117/#outline-offset0
+ * @spec http://www.w3.org/TR/2015/CR-css-ui-3-20150707/#propdef-outline-offset
  */
 public class CssOutlineOffset extends org.w3c.css.properties.css.CssOutlineOffset {
 
