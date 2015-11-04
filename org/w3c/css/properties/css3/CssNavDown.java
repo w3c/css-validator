94d25847ae7a31302bd3020edfbff4e5441bd70a
Yves Lafon <ylafon@w3.org>
2015-11-04 17:09:23 +0100

    conforms to http://www.w3.org/TR/2015/CR-css-ui-3-20150707/#propdef-nav-down @spec updated

--- org/w3c/css/properties/css3/CssNavDown.java
+++ org/w3c/css/properties/css3/CssNavDown.java
@@ -1,7 +1,7 @@
-// $Id$
+//
 // Author: Yves Lafon <ylafon@w3.org>
 //
-// (c) COPYRIGHT MIT, ERCIM and Keio University, 2012.
+// (c) COPYRIGHT MIT, ERCIM, Keio, Beihang, 2015.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.properties.css3;
 
@@ -11,7 +11,7 @@ import org.w3c.css.values.CssExpression;
 import org.w3c.css.values.CssIdent;
 
 /**
- * @spec http://www.w3.org/TR/2012/WD-css3-ui-20120117/#nav-down0
+ * @spec http://www.w3.org/TR/2015/CR-css-ui-3-20150707/#propdef-nav-down
  * @see CssNavUp
  */
 public class CssNavDown extends org.w3c.css.properties.css.CssNavDown {
@@ -55,6 +55,6 @@ public class CssNavDown extends org.w3c.css.properties.css.CssNavDown {
 	}
 
 	public boolean isDefault() {
-		return (auto == value) || (auto == initial);
+		return (auto == value) || (value == initial);
 	}
 }
