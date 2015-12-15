11f21ee0b6f118d11c17e0e4ffcb62ad23753777
Yves Lafon <ylafon@w3.org>
2015-12-15 22:10:13 +0100

    moved some classes around to allow more atRules using the same hierarchy as in 'properties'

--- org/w3c/css/atrules/css21/AtRuleMedia.java
+++ org/w3c/css/atrules/css21/AtRuleMedia.java
@@ -3,10 +3,10 @@
 // (c) COPYRIGHT MIT, ERCIM and Keio University, 2011
 // Please first read the full copyright statement in file COPYRIGHT.html
 
-package org.w3c.css.media.css21;
+package org.w3c.css.atrules.css21;
 
-import org.w3c.css.media.Media;
-import org.w3c.css.media.MediaFeature;
+import org.w3c.css.atrules.css.media.Media;
+import org.w3c.css.atrules.css.media.MediaFeature;
 import org.w3c.css.parser.AtRule;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 /**
  * @spec http://www.w3.org/TR/2011/REC-CSS2-20110607/media.html#media-types
  */
-public class AtRuleMedia extends org.w3c.css.media.AtRuleMedia {
+public class AtRuleMedia extends org.w3c.css.atrules.css.AtRuleMedia {
 
     static final String[] mediaCSS21 = {
             "all", "braille", "embossed", "handheld", "print", "projection",
@@ -32,7 +32,7 @@ public class AtRuleMedia extends org.w3c.css.media.AtRuleMedia {
      *
      * @throws InvalidParamException the medium doesn't exist
      */
-    public org.w3c.css.media.AtRuleMedia addMedia(String restrictor, String medium,
+    public org.w3c.css.atrules.css.AtRuleMedia addMedia(String restrictor, String medium,
                                                   ApplContext ac) throws InvalidParamException {
         if (restrictor != null) {
             // media restrictor not supported in CSS21
@@ -80,7 +80,7 @@ public class AtRuleMedia extends org.w3c.css.media.AtRuleMedia {
      */
     public boolean canApply(AtRule atRule) {
         try {
-            org.w3c.css.media.AtRuleMedia second = (org.w3c.css.media.AtRuleMedia) atRule;
+            org.w3c.css.atrules.css.AtRuleMedia second = (org.w3c.css.atrules.css.AtRuleMedia) atRule;
             return (canMatch(second) && second.canMatch(this));
         } catch (ClassCastException cce) {
             return false;
@@ -92,7 +92,7 @@ public class AtRuleMedia extends org.w3c.css.media.AtRuleMedia {
      */
     public boolean canMatch(AtRule atRule) {
         try {
-            org.w3c.css.media.AtRuleMedia second = (org.w3c.css.media.AtRuleMedia) atRule;
+            org.w3c.css.atrules.css.AtRuleMedia second = (org.w3c.css.atrules.css.AtRuleMedia) atRule;
             ArrayList<Media> otherMediaList = second.getMediaList();
 
             for (Media m : otherMediaList) {
--- org/w3c/css/atrules/css3/media/MediaDeviceWidth.java
+++ org/w3c/css/atrules/css3/media/MediaDeviceWidth.java
@@ -3,9 +3,9 @@
 // (c) COPYRIGHT MIT, ECRIM and Keio University, 2011
 // Please first read the full copyright statement in file COPYRIGHT.html
 
-package org.w3c.css.media.css3;
+package org.w3c.css.atrules.css3.media;
 
-import org.w3c.css.media.MediaFeature;
+import org.w3c.css.atrules.css.media.MediaFeature;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 import org.w3c.css.values.CssExpression;
@@ -15,28 +15,29 @@ import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
 
 /**
- * @spec http://www.w3.org/TR/2012/REC-css3-mediaqueries-20120619/#width
+ * @spec http://www.w3.org/TR/2012/REC-css3-mediaqueries-20120619/#device-width
  */
-public class MediaWidth extends MediaFeature {
+public class MediaDeviceWidth extends MediaFeature {
 
     /**
      * Create a new MediaWidth
      */
-    public MediaWidth() {
+    public MediaDeviceWidth() {
     }
 
     /**
      * Create a new MediaWidth.
      *
      * @param expression The expression for this media feature
-     * @throws InvalidParamException Values are incorrect
+     * @throws org.w3c.css.util.InvalidParamException
+     *          Values are incorrect
      */
-    public MediaWidth(ApplContext ac, String modifier,
-                      CssExpression expression, boolean check)
+    public MediaDeviceWidth(ApplContext ac, String modifier,
+                            CssExpression expression, boolean check)
             throws InvalidParamException {
 
         if (expression != null) {
-            if (check && expression.getCount() > 1) {
+            if (expression.getCount() > 1) {
                 throw new InvalidParamException("unrecognize", ac);
             }
 
@@ -68,7 +69,7 @@ public class MediaWidth extends MediaFeature {
         }
     }
 
-    public MediaWidth(ApplContext ac, String modifier, CssExpression expression)
+    public MediaDeviceWidth(ApplContext ac, String modifier, CssExpression expression)
             throws InvalidParamException {
         this(ac, modifier, expression, false);
     }
@@ -85,7 +86,7 @@ public class MediaWidth extends MediaFeature {
      * Returns the name of this media feature.
      */
     public final String getFeatureName() {
-        return "width";
+        return "device-width";
     }
 
     /**
@@ -95,9 +96,9 @@ public class MediaWidth extends MediaFeature {
      */
     public boolean equals(MediaFeature other) {
         try {
-            MediaWidth mw = (MediaWidth) other;
-            return (((value == null) && (mw.value == null)) || ((value != null) && value.equals(mw.value)))
-                    && (((modifier == null) && (mw.modifier == null)) || ((modifier != null) && modifier.equals(mw.modifier)));
+            MediaDeviceWidth mdw = (MediaDeviceWidth) other;
+            return (((value == null) && (mdw.value == null)) || ((value != null) && value.equals(mdw.value)))
+                    && (((modifier == null) && (mdw.modifier == null)) || ((modifier != null) && modifier.equals(mdw.modifier)));
         } catch (ClassCastException cce) {
             return false;
         }
