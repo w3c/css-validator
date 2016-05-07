ff8021e8e346c62db0ad242cf7e48b0a1f4e41d9
Yves Lafon <ylafon@w3.org>
2016-05-07 19:08:00 +0200

    icon is deprecated (it may be removed later)

--- org/w3c/css/properties/css3/CssIcon.java
+++ org/w3c/css/properties/css3/CssIcon.java
@@ -19,6 +19,7 @@ import static org.w3c.css.values.CssOperator.COMMA;
 
 /**
  * @spec http://www.w3.org/TR/2012/WD-css3-ui-20120117/#icon
+ * @deprecated
  */
 public class CssIcon extends org.w3c.css.properties.css.CssIcon {
 
