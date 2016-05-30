2c231c4da2963e512c5b04b13d20ecd53f3e4092
Yves Lafon <ylafon@w3.org>
2016-05-30 14:54:43 +0200

    rewrote most of it to depend directly on CssWidth, per spec

--- org/w3c/css/properties/css3/CssFlexBasis.java
+++ org/w3c/css/properties/css3/CssFlexBasis.java
@@ -1,31 +1,30 @@
-// $Id$
+//
 // Author: Yves Lafon <ylafon@w3.org>
 //
-// (c) COPYRIGHT MIT, ERCIM and Keio University, 2012.
+// (c) COPYRIGHT MIT, ERCIM, Keio, Beihang, 2016.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.properties.css3;
 
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
-import org.w3c.css.values.CssCheckableValue;
 import org.w3c.css.values.CssExpression;
 import org.w3c.css.values.CssIdent;
 import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
 
 /**
- * @spec http://www.w3.org/TR/2014/WD-css-flexbox-1-20140925/#propdef-flex-basis
+ * @spec https://www.w3.org/TR/2016/CR-css-flexbox-1-20160526/#propdef-flex-basis
  * @see CssWidth
  */
 public class CssFlexBasis extends org.w3c.css.properties.css.CssFlexBasis {
 
-	public static final CssIdent main_size = CssIdent.getIdent("main-size");
+	public static final CssIdent content = CssIdent.getIdent("content");
 
 	public static final CssIdent getAllowedIdent(CssIdent ident) {
-		if (main_size.equals(ident)) {
-			return main_size;
+		if (content.equals(ident)) {
+			return content;
 		}
-		return null;
+		return CssWidth.getAllowedIdent(ident);
 	}
 
 	/**
@@ -48,37 +47,29 @@ public class CssFlexBasis extends org.w3c.css.properties.css.CssFlexBasis {
 			throw new InvalidParamException("unrecognize", ac);
 		}
 
-		/**
-		 * WARNING - this is a replication of CSS3 Width parsing
-		 * instead of creating a new object. This is because the exception
-		 * would be tight to width's getPropertyName()
-		 */
-
 		CssValue val = expression.getValue();
 
 		setByUser();
 
 		switch (val.getType()) {
 			case CssTypes.CSS_IDENT:
-				CssIdent ident = (CssIdent) val;
 				if (inherit.equals(val)) {
 					value = inherit;
-				} else if (main_size.equals(val)) {
-					value = main_size;
-				} else {
-					throw new InvalidParamException("unrecognize", ac);
+					break;
+				} else if (content.equals(val)) {
+					value = content;
+					break;
 				}
-				break;
-			case CssTypes.CSS_NUMBER:
-				val.getLength();
-			case CssTypes.CSS_LENGTH:
-			case CssTypes.CSS_PERCENTAGE:
-				CssCheckableValue l = val.getCheckableValue();
-				l.checkPositiveness(ac, this);
-				value = l;
-				break;
 			default:
-				throw new InvalidParamException("value", val, getPropertyName(), ac);
+				// content or else CssWidth
+				CssExpression e = new CssExpression();
+				e.addValue(val);
+				try {
+					CssWidth cssWidth = new CssWidth(ac, e, check);
+					value = cssWidth.value;
+				} catch (InvalidParamException ex) {
+					throw new InvalidParamException("value", val, getPropertyName(), ac);
+				}
 		}
 		expression.next();
 	}
@@ -93,7 +84,7 @@ public class CssFlexBasis extends org.w3c.css.properties.css.CssFlexBasis {
 	 * It is used by all macro for the function <code>print</code>
 	 */
 	public boolean isDefault() {
-		return ((value == main_size) || (value == initial));
+		return ((value == content) || (value == initial));
 	}
 }
 
