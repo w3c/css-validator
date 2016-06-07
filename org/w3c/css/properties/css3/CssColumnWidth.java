6b777fa0425790af2a293a6100552c3105a32edf
Yves Lafon <ylafon@w3.org>
2016-06-07 14:12:35 +0200

    added extra values per https://www.w3.org/TR/2016/WD-css-sizing-3-20160512/#column-sizing

--- org/w3c/css/properties/css3/CssColumnWidth.java
+++ org/w3c/css/properties/css3/CssColumnWidth.java
@@ -1,10 +1,9 @@
-// $Id$
+//
 // From Sijtsche de Jong (sy.de.jong@let.rug.nl)
 // Rewriten 2010 Yves Lafon <ylafon@w3.org>
 //
-// (c) COPYRIGHT 1995-2010  World Wide Web Consortium (MIT, ERCIM and Keio)
-// Please first read the full copyright statement at
-// http://www.w3.org/Consortium/Legal/copyright-software-19980720
+// (c) COPYRIGHT 1995-2010  World Wide Web Consortium (MIT, ERCIM, Keio, Beihang)
+// Please first read the full copyright statement in file COPYRIGHT.html
 
 package org.w3c.css.properties.css3;
 
@@ -18,14 +17,31 @@ import org.w3c.css.values.CssValue;
 
 /**
  * @spec http://www.w3.org/TR/2011/CR-css3-multicol-20110412/#column-width
+ * @spec https://www.w3.org/TR/2016/WD-css-sizing-3-20160512/#column-sizing
  */
 
 public class CssColumnWidth extends org.w3c.css.properties.css.CssColumnWidth {
 
-	static CssIdent auto;
+	public static final CssIdent[] allowed_values;
 
 	static {
-		auto = CssIdent.getIdent("auto");
+		// fill to fit-content from css-sizing
+		String[] _allowed_values = {"auto", "fill", "max-content", "min-content", "fit-content"};
+
+		allowed_values = new CssIdent[_allowed_values.length];
+		int i = 0;
+		for (String s : _allowed_values) {
+			allowed_values[i++] = CssIdent.getIdent(s);
+		}
+	}
+
+	public static CssIdent getAllowedIdent(CssIdent ident) {
+		for (CssIdent id : allowed_values) {
+			if (id.equals(ident)) {
+				return id;
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -67,11 +83,16 @@ public class CssColumnWidth extends org.w3c.css.properties.css.CssColumnWidth {
 			case CssTypes.CSS_IDENT:
 				if (inherit.equals(val)) {
 					value = inherit;
-					break;
-				} else if (auto.equals(val)) {
-					value = auto;
-					break;
+				} else {
+					CssIdent id = getAllowedIdent((CssIdent) val);
+					if (id != null) {
+						value = id;
+					} else {
+						throw new InvalidParamException("unrecognize", ac);
+					}
 				}
+				break;
+
 			default:
 				throw new InvalidParamException("value", expression.getValue(),
 						getPropertyName(), ac);
@@ -88,8 +109,9 @@ public class CssColumnWidth extends org.w3c.css.properties.css.CssColumnWidth {
 	 * Is the value of this property a default value
 	 * It is used by all macro for the function <code>print</code>
 	 */
+
 	public boolean isDefault() {
-		return (auto == initial);
+		return (value == initial);
 	}
 
 }
