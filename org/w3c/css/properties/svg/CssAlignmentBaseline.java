619aec33af93062eb8155f8fb516f1988ab690d7
Yves Lafon <ylafon@w3.org>
2016-07-30 11:30:13 +0200

    In fact, it's better for maintenance to rely on css3's getAllowedIdent, even if slightly less efficient

--- org/w3c/css/properties/svg/CssAlignmentBaseline.java
+++ org/w3c/css/properties/svg/CssAlignmentBaseline.java
@@ -14,44 +14,24 @@ import org.w3c.css.values.CssValue;
 
 /**
  * @spec http://www.w3.org/TR/2011/REC-SVG11-20110816/text.html#AlignmentBaselineProperty
- * @spec http://www.w3.org/TR/2015/WD-css-inline-3-20150917/#alignment-baseline-property
  */
 public class CssAlignmentBaseline extends org.w3c.css.properties.css.CssAlignmentBaseline {
 
-	public static final CssIdent[] svg_allowed_values;
-	public static final CssIdent[] css3_allowed_values;
+	public static final CssIdent[] allowed_values;
 
 	static {
 		String[] _allowed_values = {"auto", "baseline", "before-edge", "text-before-edge",
 				"middle", "central", "after-edge", "text-after-edge", "ideographic",
 				"alphabetic", "hanging", "mathematical"};
 		int i = 0;
-		svg_allowed_values = new CssIdent[_allowed_values.length];
+		allowed_values = new CssIdent[_allowed_values.length];
 		for (String s : _allowed_values) {
-			svg_allowed_values[i++] = CssIdent.getIdent(s);
-		}
-
-		String[] _allowed_css3_values = {"baseline", "text-bottom", "alphabetic", "middle",
-				"central", "mathematical", "text-top", "bottom", "center", "top"};
-		i = 0;
-		css3_allowed_values = new CssIdent[_allowed_css3_values.length];
-		boolean add_it;
-		for (String s : _allowed_css3_values) {
-			// get only the ones not defined by SVG
-			add_it = false;
-			for (String other : _allowed_values) {
-				add_it = other.equals(s);
-				if (add_it)
-					break;
-			}
-			if (add_it) {
-				css3_allowed_values[i++] = CssIdent.getIdent(s);
-			}
+			allowed_values[i++] = CssIdent.getIdent(s);
 		}
 	}
 
 	public static final CssIdent getAllowedIdent(CssIdent ident) {
-		for (CssIdent id : svg_allowed_values) {
+		for (CssIdent id : allowed_values) {
 			if (id.equals(ident)) {
 				return id;
 			}
@@ -59,14 +39,6 @@ public class CssAlignmentBaseline extends org.w3c.css.properties.css.CssAlignmen
 		return null;
 	}
 
-	public static final CssIdent getAllowedCSS3Ident(CssIdent ident) {
-		for (CssIdent id : css3_allowed_values) {
-			if (id.equals(ident)) {
-				return id;
-			}
-		}
-		return null;
-	}
 
 	/**
 	 * Create a new CssAlignmentBaseline
@@ -102,7 +74,7 @@ public class CssAlignmentBaseline extends org.w3c.css.properties.css.CssAlignmen
 				if (value == null) {
 					// we also check if CSS version is CSS3 and onward.
 					if (ac.getCssVersion().compareTo(CssVersion.CSS3) >= 0) {
-						value = getAllowedCSS3Ident(id);
+						value = org.w3c.css.properties.css3.CssAlignmentBaseline.getAllowedIdent(id);
 					}
 				}
 				if (value == null) {
