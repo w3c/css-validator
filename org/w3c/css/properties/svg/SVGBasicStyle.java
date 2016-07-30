458d2c8bf564537ecf23fd4bd895f0ffa3c88776
Yves Lafon <ylafon@w3.org>
2016-07-30 11:35:01 +0200

    added dominant-baseline that checks also for css3 values when SVG is used with CSS3, see <http://www.w3.org/TR/2011/REC-SVG11-20110816/text.html#DominantBaselineProperty> - removed old implementation

--- org/w3c/css/properties/svg/SVGBasicStyle.java
+++ org/w3c/css/properties/svg/SVGBasicStyle.java
@@ -10,215 +10,205 @@ package org.w3c.css.properties.svg;
 
 public class SVGBasicStyle extends SVGTinyStyle {
 
-    ClipPath clipPath;
-    ClipRule clipRule;
-    ColorInterpolation colorInterpolation;
-    ColorRendering colorRendering;
-    EnableBackground enableBackground;
-    WritingModeSVG writingModeSVG;
-    FillOpacity fillOpacity;
-    ImageRendering imageRendering;
-    Mask mask;
-    StopOpacity stopOpacity;
-    Kerning kerning;
-    PointerEvents pointerEvents;
-    ShapeRendering shapeRendering;
-    TextRendering textRendering;
-    TextAnchor textAnchor;
-    StrokeOpacity strokeOpacity;
-    StopColor stopColor;
-    SolidColor solidColor;
-    ColorProfile colorProfile;
-    DominantBaseLine dominantBaseLine;
-    SolidOpacity solidOpacity;
+	ClipPath clipPath;
+	ClipRule clipRule;
+	ColorInterpolation colorInterpolation;
+	ColorRendering colorRendering;
+	EnableBackground enableBackground;
+	WritingModeSVG writingModeSVG;
+	FillOpacity fillOpacity;
+	ImageRendering imageRendering;
+	Mask mask;
+	StopOpacity stopOpacity;
+	Kerning kerning;
+	PointerEvents pointerEvents;
+	ShapeRendering shapeRendering;
+	TextRendering textRendering;
+	TextAnchor textAnchor;
+	StrokeOpacity strokeOpacity;
+	StopColor stopColor;
+	SolidColor solidColor;
+	ColorProfile colorProfile;
+	SolidOpacity solidOpacity;
 
-    public DominantBaseLine getDominantBaseLineSVG() {
-	if (dominantBaseLine == null) {
-	    dominantBaseLine =
-		(DominantBaseLine) style.CascadingOrder (
-		    new DominantBaseLine(), style, selector);
+	public ClipPath getClipPath() {
+		if (clipPath == null) {
+			clipPath =
+					(ClipPath) style.CascadingOrder(
+							new ClipPath(), style, selector);
+		}
+		return clipPath;
 	}
-	return dominantBaseLine;
-    }
 
-    public ClipPath getClipPath() {
-	if (clipPath == null) {
-	    clipPath =
-		(ClipPath) style.CascadingOrder (
-		    new ClipPath(), style, selector);
+	public ClipRule getClipRule() {
+		if (clipRule == null) {
+			clipRule =
+					(ClipRule) style.CascadingOrder(
+							new ClipRule(), style, selector);
+		}
+		return clipRule;
 	}
-	return clipPath;
-    }
 
-    public ClipRule getClipRule() {
-	if (clipRule == null) {
-	    clipRule =
-		(ClipRule) style.CascadingOrder (
-		    new ClipRule(), style, selector);
+	public ColorInterpolation getColorInterpolation() {
+		if (colorInterpolation == null) {
+			colorInterpolation =
+					(ColorInterpolation) style.CascadingOrder(
+							new ColorInterpolation(), style, selector);
+		}
+		return colorInterpolation;
 	}
-	return clipRule;
-    }
 
-    public ColorInterpolation getColorInterpolation() {
-	if (colorInterpolation == null) {
-	    colorInterpolation =
-		(ColorInterpolation) style.CascadingOrder (
-		    new ColorInterpolation(), style, selector);
+	public ColorRendering getColorRendering() {
+		if (colorRendering == null) {
+			colorRendering =
+					(ColorRendering) style.CascadingOrder(
+							new ColorRendering(), style, selector);
+		}
+		return colorRendering;
 	}
-	return colorInterpolation;
-    }
 
-    public ColorRendering getColorRendering() {
-	if (colorRendering == null) {
-	    colorRendering =
-		(ColorRendering) style.CascadingOrder (
-		    new ColorRendering(), style, selector);
+	public EnableBackground getEnableBackground() {
+		if (enableBackground == null) {
+			enableBackground =
+					(EnableBackground) style.CascadingOrder(
+							new EnableBackground(), style, selector);
+		}
+		return enableBackground;
 	}
-	return colorRendering;
-    }
 
-    public EnableBackground getEnableBackground() {
-	if (enableBackground == null) {
-	    enableBackground =
-		(EnableBackground) style.CascadingOrder (
-		    new EnableBackground(), style, selector);
+	public WritingModeSVG getWritingModeSVG() {
+		if (writingModeSVG == null) {
+			writingModeSVG =
+					(WritingModeSVG) style.CascadingOrder(
+							new WritingModeSVG(), style, selector);
+		}
+		return writingModeSVG;
 	}
-	return enableBackground;
-    }
 
-    public WritingModeSVG getWritingModeSVG() {
-	if (writingModeSVG == null) {
-	    writingModeSVG =
-		(WritingModeSVG) style.CascadingOrder (
-		    new WritingModeSVG(), style, selector);
+	public FillOpacity getFillOpacity() {
+		if (fillOpacity == null) {
+			fillOpacity =
+					(FillOpacity) style.CascadingOrder(
+							new FillOpacity(), style, selector);
+		}
+		return fillOpacity;
 	}
-	return writingModeSVG;
-    }
 
-    public FillOpacity getFillOpacity() {
-	if (fillOpacity == null) {
-	    fillOpacity =
-		(FillOpacity) style.CascadingOrder (
-		    new FillOpacity(), style, selector);
+	public ImageRendering getImageRendering() {
+		if (imageRendering == null) {
+			imageRendering =
+					(ImageRendering) style.CascadingOrder(
+							new ImageRendering(), style, selector);
+		}
+		return imageRendering;
 	}
-	return fillOpacity;
-    }
 
-    public ImageRendering getImageRendering() {
-	if (imageRendering == null) {
-	    imageRendering =
-		(ImageRendering) style.CascadingOrder (
-		    new ImageRendering(), style, selector);
+	public Mask getMask() {
+		if (mask == null) {
+			mask =
+					(Mask) style.CascadingOrder(
+							new Mask(), style, selector);
+		}
+		return mask;
 	}
-	return imageRendering;
-    }
 
-    public Mask getMask() {
-	if (mask == null) {
-	    mask =
-		(Mask) style.CascadingOrder (
-		    new Mask(), style, selector);
+	public StopOpacity getStopOpacity() {
+		if (stopOpacity == null) {
+			stopOpacity =
+					(StopOpacity) style.CascadingOrder(
+							new StopOpacity(), style, selector);
+		}
+		return stopOpacity;
 	}
-	return mask;
-    }
 
-    public StopOpacity getStopOpacity() {
-	if (stopOpacity == null) {
-	    stopOpacity =
-		(StopOpacity) style.CascadingOrder (
-		    new StopOpacity(), style, selector);
+	public Kerning getKerning() {
+		if (kerning == null) {
+			kerning =
+					(Kerning) style.CascadingOrder(
+							new Kerning(), style, selector);
+		}
+		return kerning;
 	}
-	return stopOpacity;
-    }
 
-    public Kerning getKerning() {
-	if (kerning == null) {
-	    kerning =
-		(Kerning) style.CascadingOrder (
-		    new Kerning(), style, selector);
+	public PointerEvents getPointerEvents() {
+		if (pointerEvents == null) {
+			pointerEvents =
+					(PointerEvents) style.CascadingOrder(
+							new PointerEvents(), style, selector);
+		}
+		return pointerEvents;
 	}
-	return kerning;
-    }
 
-    public PointerEvents getPointerEvents() {
-	if (pointerEvents == null) {
-	    pointerEvents =
-		(PointerEvents) style.CascadingOrder (
-		    new PointerEvents(), style, selector);
+	public ShapeRendering getShapeRendering() {
+		if (shapeRendering == null) {
+			shapeRendering =
+					(ShapeRendering) style.CascadingOrder(
+							new ShapeRendering(), style, selector);
+		}
+		return shapeRendering;
 	}
-	return pointerEvents;
-    }
 
-    public ShapeRendering getShapeRendering() {
-	if (shapeRendering == null) {
-	    shapeRendering =
-		(ShapeRendering) style.CascadingOrder (
-		    new ShapeRendering(), style, selector);
+	public TextRendering getTextRendering() {
+		if (textRendering == null) {
+			textRendering =
+					(TextRendering) style.CascadingOrder(
+							new TextRendering(), style, selector);
+		}
+		return textRendering;
 	}
-	return shapeRendering;
-    }
 
-    public TextRendering getTextRendering() {
-	if (textRendering == null) {
-	    textRendering =
-		(TextRendering) style.CascadingOrder (
-		    new TextRendering(), style, selector);
+	public TextAnchor getTextAnchor() {
+		if (textAnchor == null) {
+			textAnchor =
+					(TextAnchor) style.CascadingOrder(
+							new TextAnchor(), style, selector);
+		}
+		return textAnchor;
 	}
-	return textRendering;
-    }
 
-    public TextAnchor getTextAnchor() {
-	if (textAnchor == null) {
-	    textAnchor =
-		(TextAnchor) style.CascadingOrder (
-		    new TextAnchor(), style, selector);
+	public StrokeOpacity getStrokeOpacity() {
+		if (strokeOpacity == null) {
+			strokeOpacity =
+					(StrokeOpacity) style.CascadingOrder(
+							new StrokeOpacity(), style, selector);
+		}
+		return strokeOpacity;
 	}
-	return textAnchor;
-    }
 
-    public StrokeOpacity getStrokeOpacity() {
-	if (strokeOpacity == null) {
-	    strokeOpacity =
-		(StrokeOpacity) style.CascadingOrder (
-		    new StrokeOpacity(), style, selector);
+	public StopColor getStopColor() {
+		if (stopColor == null) {
+			stopColor =
+					(StopColor) style.CascadingOrder(
+							new StopColor(), style, selector);
+		}
+		return stopColor;
 	}
-	return strokeOpacity;
-    }
 
-    public StopColor getStopColor() {
-	if (stopColor == null) {
-	    stopColor =
-		(StopColor) style.CascadingOrder (
-		    new StopColor(), style, selector);
+	public SolidColor getSolidColor() {
+		if (solidColor == null) {
+			solidColor =
+					(SolidColor) style.CascadingOrder(
+							new SolidColor(), style, selector);
+		}
+		return solidColor;
 	}
-	return stopColor;
-    }
 
-    public SolidColor getSolidColor() {
-	if (solidColor == null) {
-	    solidColor =
-		(SolidColor) style.CascadingOrder (
-		    new SolidColor(), style, selector);
+	public ColorProfile getColorProfileSVG() {
+		if (colorProfile == null) {
+			colorProfile =
+					(ColorProfile) style.CascadingOrder(
+							new ColorProfile(), style, selector);
+		}
+		return colorProfile;
 	}
-	return solidColor;
-    }
 
-    public ColorProfile getColorProfileSVG() {
-	if (colorProfile == null) {
-	    colorProfile =
-		(ColorProfile) style.CascadingOrder (
-		    new ColorProfile(), style, selector);
+	public SolidOpacity getSolidOpacity() {
+		if (solidOpacity == null) {
+			solidOpacity =
+					(SolidOpacity) style.CascadingOrder(
+							new SolidOpacity(), style, selector);
+		}
+		return solidOpacity;
 	}
-	return colorProfile;
-    }
-
-    public SolidOpacity getSolidOpacity() {
-	if (solidOpacity == null) {
-	    solidOpacity =
-		(SolidOpacity) style.CascadingOrder (
-		    new SolidOpacity(), style, selector);
-	}
-	return solidOpacity;
-    }
 
 }
