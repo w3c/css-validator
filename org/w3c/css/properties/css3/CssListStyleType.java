96af67506f5ea4c97daac6b659464a441f5e66f7
Yves Lafon <ylafon@w3.org>
2017-02-07 18:12:31 +0100

    added symbols() value to list-stype-type per https://www.w3.org/TR/2015/CR-css-counter-styles-3-20150611/#funcdef-symbols however its definition needs to be updated to match the latest version

--- org/w3c/css/properties/css3/CssListStyleType.java
+++ org/w3c/css/properties/css3/CssListStyleType.java
@@ -5,21 +5,28 @@
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.properties.css3;
 
+import org.w3c.css.properties.css.CssProperty;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 import org.w3c.css.values.CssExpression;
+import org.w3c.css.values.CssFunction;
 import org.w3c.css.values.CssIdent;
 import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
 
 import java.util.Arrays;
 
+import static org.w3c.css.values.CssOperator.SPACE;
+
 /**
  * @spec http://www.w3.org/TR/2011/WD-css3-lists-20110524/#list-style-type
+ * @spec https://www.w3.org/TR/2015/CR-css-counter-styles-3-20150611/#funcdef-symbols
  */
 public class CssListStyleType extends org.w3c.css.properties.css.CssListStyleType {
 
 	public static final CssIdent[] allowed_values;
+
+	public static CssIdent[] symbols_type;
 
 	static {
 		String[] _allowed_values = {"none", "inline", "ethiopic-numeric",
@@ -69,11 +76,28 @@ public class CssListStyleType extends org.w3c.css.properties.css.CssListStyleTyp
 			allowed_values[i++] = CssIdent.getIdent(s);
 		}
 		Arrays.sort(allowed_values);
+
+		String[] _allowed_symbols_type_values = {"cyclic", "numeric",
+				"alphabetic", "symbolic", "fixed"};
+		symbols_type = new CssIdent[_allowed_symbols_type_values.length];
+		i = 0;
+		for (String s : _allowed_symbols_type_values) {
+			symbols_type[i++] = CssIdent.getIdent(s);
+		}
 	}
 
 	public static final CssIdent getAllowedIdent(CssIdent ident) {
 		int idx = Arrays.binarySearch(allowed_values, ident);
 		return (idx < 0) ? null : allowed_values[idx];
+	}
+
+	public static CssIdent getSymbolsFunctionAllowedIdent(CssIdent ident) {
+		for (CssIdent id : symbols_type) {
+			if (id.equals(ident)) {
+				return id;
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -119,6 +143,9 @@ public class CssListStyleType extends org.w3c.css.properties.css.CssListStyleTyp
 		op = expression.getOperator();
 
 		switch (val.getType()) {
+			case CssTypes.CSS_FUNCTION:
+				value = parseSymbolsFunction(ac, (CssFunction) val, this);
+				break;
 			case CssTypes.CSS_STRING:
 				value = val;
 				break;
@@ -145,6 +172,58 @@ public class CssListStyleType extends org.w3c.css.properties.css.CssListStyleTyp
 						getPropertyName(), ac);
 		}
 		expression.next();
+	}
+
+	/**
+	 * @spec https://www.w3.org/TR/2015/CR-css-counter-styles-3-20150611/#funcdef-symbols
+	 */
+	protected static CssFunction parseSymbolsFunction(ApplContext ac, CssFunction function,
+													  CssProperty caller)
+			throws InvalidParamException {
+		CssExpression exp = function.getParameters();
+		CssValue val;
+		char op;
+		boolean got_symbol = false;
+
+		if (!"symbols".equals(function.getName())) {
+			throw new InvalidParamException("unrecognize", ac);
+		}
+		while (!exp.end()) {
+			val = exp.getValue();
+			op = exp.getOperator();
+
+			switch (val.getType()) {
+				case CssTypes.CSS_IDENT:
+					if (got_symbol) {
+						throw new InvalidParamException("value", val,
+								caller.getPropertyName(), ac);
+					}
+					CssIdent ident = getSymbolsFunctionAllowedIdent((CssIdent) val);
+					if (ident == null) {
+						throw new InvalidParamException("value", val,
+								caller.getPropertyName(), ac);
+					}
+					// we are not storing values
+					break;
+				case CssTypes.CSS_URL:
+				case CssTypes.CSS_IMAGE:
+				case CssTypes.CSS_STRING:
+					got_symbol = true;
+					break;
+				default:
+					throw new InvalidParamException("value", val,
+							caller.getPropertyName(), ac);
+			}
+			if (op != SPACE) {
+				throw new InvalidParamException("operator", op,
+						caller.getPropertyName(), ac);
+			}
+			exp.next();
+		}
+		if (!got_symbol) {
+			throw new InvalidParamException("unrecognize", ac);
+		}
+		return function;
 	}
 
 }
