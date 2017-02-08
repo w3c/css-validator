b395bb3528f7b7d77fef0db03230484be31c8c94
Yves Lafon <ylafon@w3.org>
2017-02-08 21:28:59 +0100

    aligned list-style-type with https://www.w3.org/TR/2014/WD-css-lists-3-20140320/#propdef-list-style-type

--- org/w3c/css/properties/css3/CssListStyleType.java
+++ org/w3c/css/properties/css3/CssListStyleType.java
@@ -1,7 +1,7 @@
-// $Id$
+//
 // Author: Yves Lafon <ylafon@w3.org>
 //
-// (c) COPYRIGHT MIT, ERCIM and Keio University, 2012.
+// (c) COPYRIGHT MIT, ERCIM, Keio, Beihang University, 2017.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.properties.css3;
 
@@ -19,8 +19,8 @@ import java.util.Arrays;
 import static org.w3c.css.values.CssOperator.SPACE;
 
 /**
- * @spec http://www.w3.org/TR/2011/WD-css3-lists-20110524/#list-style-type
- * @spec https://www.w3.org/TR/2015/CR-css-counter-styles-3-20150611/#funcdef-symbols
+ * @spec https://www.w3.org/TR/2014/WD-css-lists-3-20140320/#propdef-list-style-type
+ * @spec https://www.w3.org/TR/2015/CR-css-counter-styles-3-20150611/#typedef-counter-style
  */
 public class CssListStyleType extends org.w3c.css.properties.css.CssListStyleType {
 
@@ -29,47 +29,20 @@ public class CssListStyleType extends org.w3c.css.properties.css.CssListStyleTyp
 	public static CssIdent[] symbols_type;
 
 	static {
-		String[] _allowed_values = {"none", "inline", "ethiopic-numeric",
-				"triangle", "trinary", "go", "fixed-decimal", "unary",
-				"box-corner", "dice", "box", "check", "circle", "diamond",
-				"disc", "dash", "square", "arabic-indic", "bengali",
-				"binary", "burmese", "cambodian", "cjk-decimal", "decimal",
-				"devanagari", "eastern-nagari", "fullwidth-decimal",
-				"gujarati", "gurmukhi", "kannada", "khmer",
-				"lower-hexadecimal", "lao", "lepcha", "malayalam", "marathi",
-				"mongolian", "myanmar", "new-base-60", "octal", "oriya",
-				"persian", "super-decimal", "tamil", "telugu", "tibetan",
-				"thai", "upper-hexadecimal", "afar", "agaw", "ari", "blin",
-				"cjk-earthly-branch", "cjk-heavenly-stem", "dizi",
-				"fullwidth-lower-alpha", "fullwidth-upper-alpha",
-				"gedeo", "gumuz", "hadiyya", "harari", "hindi",
-				"hiragana-iroha", "hiragana", "kaffa", "katakana-iroha",
-				"katakana", "kebena", "kembata", "konso", "korean-consonant",
-				"korean-syllable", "kunama", "lower-alpha",
-				"lower-belorussian", "lower-bulgarian", "lower-greek",
-				"lower-macedonian", "lower-oromo-qubee", "lower-russian",
-				"lower-russian-full", "lower-serbo-croatian",
-				"lower-ukrainian", "lower-ukrainian-full",
-				"meen", "oromo", "saho", "sidama", "silti",
-				"thai-alphabetic", "tigre", "upper-alpha", "upper-belorussian",
-				"upper-bulgarian", "upper-macedonian", "upper-oromo-qubee",
-				"upper-russian", "upper-russian-full", "upper-serbo-croatian",
-				"upper-ukrainian", "upper-ukrainian-full", "wolaita",
-				"yemsa", "asterisks", "footnotes", "lower-alpha-symbolic",
-				"upper-alpha-symbolic", "circled-decimal",
-				"circled-lower-latin", "circled-upper-latin", "circled-korean-consonants",
-				"circled-korean-syllables", "decimal-leading-zero",
-				"dotted-decimal", "double-circled-decimal",
-				"filled-circled-decimal", "fullwidth-upper-roman",
-				"fullwidth-lower-roman", "parenthesized-decimal",
-				"parenthesized-lower-latin", "parenthesized-hangul-consonants",
-				"parenthesized-hangul-syllable", "persian-abjad",
-				"persian-alphabetic", "hebrew", "simple-upper-roman",
-				"simple-lower-roman", "upper-roman", "lower-roman",
-				"lower-armenian", "upper-armenian", "armenian", "georgian",
-				"ancient-tamil", "japanese-informal", "japanese-formal",
+		String[] _allowed_values = {"none", "decimal", "decimal-leading-zero",
+				"arabic-indic", "armenian", "upper-armenian", "lower-armenian",
+				"bengali", "cambodian", "khmer", "cjk-decimal", "devanagari",
+				"georgian", "gujarati", "gurmukhi", "hebrew", "kannada", "lao",
+				"malayalam", "mongolian", "myanmar", "oriya", "persian",
+				"lower-roman", "upper-roman", "tamil", "telugu", "thai",
+				"tibetan", "lower-alpha", "lower-latin", "upper-alpha",
+				"upper-latin", "cjk-earthly-branch", "cjk-heavenly-stem",
+				"lower-greek", "hiragana", "hiragana-iroha", "katakana",
+				"katakana-iroha", "disc", "circle", "square", "disclosure-open",
+				"disclosure-closed", "japanese-informal", "japanese-formal",
 				"korean-hangul-formal", "korean-hanja-informal",
-				"korean-hanja-formal", "greek"};
+				"korean-hanja-formal", "simp-chinese-informal", "simp-chinese-formal",
+				"trad-chinese-informal", "trad-chinese-formal", "ethiopic-numeric"};
 		int i = 0;
 		allowed_values = new CssIdent[_allowed_values.length];
 		for (String s : _allowed_values) {
@@ -137,10 +110,8 @@ public class CssListStyleType extends org.w3c.css.properties.css.CssListStyleTyp
 		setByUser();
 
 		CssValue val;
-		char op;
 
 		val = expression.getValue();
-		op = expression.getOperator();
 
 		switch (val.getType()) {
 			case CssTypes.CSS_FUNCTION:
@@ -151,19 +122,25 @@ public class CssListStyleType extends org.w3c.css.properties.css.CssListStyleTyp
 				break;
 			case CssTypes.CSS_IDENT:
 				CssIdent id = (CssIdent) val;
+				if (none.equals(id)) {
+					value = none;
+					break;
+				}
 				if (inherit.equals(id)) {
 					value = inherit;
 					break;
-				} else {
-					value = getAllowedIdent(id);
-					if (value == null) {
-						// it's still acceptable
-						// but the name should be listed in a
-						// @counter-style rule
-						value = val;
-						// TODO check counter-style
-						break;
+				}
+				value = getAllowedIdent(id);
+				if (value == null) {
+					// it's still acceptable
+					// but the name should be listed in a
+					// @counter-style rule
+					if (CssIdent.isCssWide(id)) {
+						throw new InvalidParamException("value",
+								val.toString(),
+								getPropertyName(), ac);
 					}
+					value = id;
 					break;
 				}
 			default:
