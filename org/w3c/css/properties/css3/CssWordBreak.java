24f60aeed28d31fd46c0be8b47d29a20e457746d
Michael[tm] Smith <mike@w3.org>
2017-12-26 16:36:42 +0900

    Allow 'word-break: break-word'
    This change allows the `break-word` value for the `word-break` property,
    per https://www.w3.org/TR/2017/WD-css-text-3-20170822/#word-break-property
    
    Fixes https://github.com/w3c/css-validator/issues/128

--- org/w3c/css/properties/css3/CssWordBreak.java
+++ org/w3c/css/properties/css3/CssWordBreak.java
@@ -20,7 +20,8 @@ public class CssWordBreak extends org.w3c.css.properties.css.CssWordBreak {
 	public static final CssIdent[] allowed_values;
 
 	static {
-		String[] _allowed_values = {"normal", "keep-all", "break-all"};
+		String[] _allowed_values = {"normal", "keep-all", "break-all",
+			"break-word"};
 		allowed_values = new CssIdent[_allowed_values.length];
 		int i = 0;
 		for (String s : _allowed_values) {
