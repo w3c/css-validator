ed3102afdc910a1a6c4aa5f3741042be748dea2a
Yves Lafon <ylafon@w3.org>
2018-01-08 15:19:02 +0100

    aligned caption side per https://www.w3.org/TR/2017/WD-css-logical-1-20170518/#caption-side

--- org/w3c/css/properties/css3/CssCaptionSide.java
+++ org/w3c/css/properties/css3/CssCaptionSide.java
@@ -14,80 +14,80 @@ import org.w3c.css.values.CssValue;
 
 /**
  * @spec http://www.w3.org/TR/2011/REC-CSS2-20110607/tables.html#propdef-caption-side
- * @spec http://www.w3.org/TR/2012/WD-css3-writing-modes-20120501/#caption-side
+ * @spec https://www.w3.org/TR/2017/WD-css-logical-1-20170518/#caption-side
  */
 public class CssCaptionSide extends org.w3c.css.properties.css.CssCaptionSide {
 
-	public static final CssIdent[] allowed_values;
+    public static final CssIdent[] allowed_values;
 
-	static {
-		String[] _allowed_values = {"top", "bottom", "before", "after"};
-		int i = 0;
-		allowed_values = new CssIdent[_allowed_values.length];
-		for (String s : _allowed_values) {
-			allowed_values[i++] = CssIdent.getIdent(s);
-		}
-	}
+    static {
+        String[] _allowed_values = {"top", "bottom", "inline-start", "inline-end"};
+        int i = 0;
+        allowed_values = new CssIdent[_allowed_values.length];
+        for (String s : _allowed_values) {
+            allowed_values[i++] = CssIdent.getIdent(s);
+        }
+    }
 
-	public static final CssIdent getAllowedIdent(CssIdent ident) {
-		for (CssIdent id : allowed_values) {
-			if (id.equals(ident)) {
-				return id;
-			}
-		}
-		return null;
-	}
+    public static final CssIdent getAllowedIdent(CssIdent ident) {
+        for (CssIdent id : allowed_values) {
+            if (id.equals(ident)) {
+                return id;
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * Create a new CssCaptionSide
-	 */
-	public CssCaptionSide() {
-		value = initial;
-	}
+    /**
+     * Create a new CssCaptionSide
+     */
+    public CssCaptionSide() {
+        value = initial;
+    }
 
-	/**
-	 * Creates a new CssCaptionSide
-	 *
-	 * @param expression The expression for this property
-	 * @throws org.w3c.css.util.InvalidParamException
-	 *          Expressions are incorrect
-	 */
-	public CssCaptionSide(ApplContext ac, CssExpression expression, boolean check)
-			throws InvalidParamException {
-		if (check && expression.getCount() > 1) {
-			throw new InvalidParamException("unrecognize", ac);
-		}
-		setByUser();
+    /**
+     * Creates a new CssCaptionSide
+     *
+     * @param expression The expression for this property
+     * @throws org.w3c.css.util.InvalidParamException
+     *          Expressions are incorrect
+     */
+    public CssCaptionSide(ApplContext ac, CssExpression expression, boolean check)
+            throws InvalidParamException {
+        if (check && expression.getCount() > 1) {
+            throw new InvalidParamException("unrecognize", ac);
+        }
+        setByUser();
 
-		CssValue val;
-		char op;
+        CssValue val;
+        char op;
 
-		val = expression.getValue();
-		op = expression.getOperator();
+        val = expression.getValue();
+        op = expression.getOperator();
 
-		if (val.getType() == CssTypes.CSS_IDENT) {
-			CssIdent id = (CssIdent) val;
-			if (inherit.equals(id)) {
-				value = inherit;
-			} else {
-				value = getAllowedIdent(id);
-				if (value == null) {
-					throw new InvalidParamException("value",
-							val.toString(),
-							getPropertyName(), ac);
-				}
-			}
-		} else {
-			throw new InvalidParamException("value",
-					val.toString(),
-					getPropertyName(), ac);
-		}
-		expression.next();
-	}
+        if (val.getType() == CssTypes.CSS_IDENT) {
+            CssIdent id = (CssIdent) val;
+            if (inherit.equals(id)) {
+                value = inherit;
+            } else {
+                value = getAllowedIdent(id);
+                if (value == null) {
+                    throw new InvalidParamException("value",
+                            val.toString(),
+                            getPropertyName(), ac);
+                }
+            }
+        } else {
+            throw new InvalidParamException("value",
+                    val.toString(),
+                    getPropertyName(), ac);
+        }
+        expression.next();
+    }
 
-	public CssCaptionSide(ApplContext ac, CssExpression expression)
-			throws InvalidParamException {
-		this(ac, expression, false);
-	}
+    public CssCaptionSide(ApplContext ac, CssExpression expression)
+            throws InvalidParamException {
+        this(ac, expression, false);
+    }
 }
 
