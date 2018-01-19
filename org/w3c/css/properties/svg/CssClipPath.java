w3c/css-validator

commit 66c715722083bef5db4dcc5ea721dbb967278412

Align clip-path checking more closely with spec
This change brings `clip-path` checking a bit closer into alignment with the
requirements in https://drafts.fxtf.org/css-masking-1/#the-clip-path.

Even with this change, the checker still doesn’t recognize circle(), ellipse()
or polygon() values for clip-path, but it does now recognize inset() values —
with the limitation of not yet recognizing the `round <‘border-radius’>`
optional argument.

org/w3c/css/properties/svg/CssClipPath.java
@@ -5,14 +5,18 @@
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.properties.svg;
 
+import org.w3c.css.properties.css.CssProperty;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 import org.w3c.css.values.CssExpression;
+import org.w3c.css.values.CssFunction;
 import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
 
+import static org.w3c.css.values.CssOperator.SPACE;
+
 /**
- * @spec http://www.w3.org/TR/2011/REC-SVG11-20110816/masking.html#ClipPathProperty
+ * @spec https://www.w3.org/TR/2014/CR-css-masking-1-20140826/#the-clip-path
  */
 public class CssClipPath extends org.w3c.css.properties.css.CssClipPath {
 
@@ -38,12 +42,21 @@ public class CssClipPath extends org.w3c.css.properties.css.CssClipPath {
 		setByUser();
 
 		CssValue val;
-		char op;
 
 		val = expression.getValue();
-		op = expression.getOperator();
 
 		switch (val.getType()) {
+			case CssTypes.CSS_FUNCTION:
+				CssFunction func = (CssFunction) val;
+				String funcname = func.getName().toLowerCase();
+				if (funcname.equals("inset")) {
+					checkInset(ac, func.getParameters(), this);
+				} else {
+					throw new InvalidParamException("value", val,
+							getPropertyName(), ac);
+				}
+				value = val;
+				break;
 			case CssTypes.CSS_URL:
 				value = val;
 				break;
@@ -69,5 +82,32 @@ public class CssClipPath extends org.w3c.css.properties.css.CssClipPath {
 		this(ac, expression, false);
 	}
 
+	static void checkInset(ApplContext ac, CssExpression expression,
+						   CssProperty caller) throws InvalidParamException {
+		if (expression.getCount() > 4) {
+			throw new InvalidParamException("unrecognize", ac);
+		}
+		CssValue val;
+		char op;
+		for (int i = 0; i < 4; i++) {
+			val = expression.getValue();
+			op = expression.getOperator();
+			if (op != SPACE) {
+				throw new InvalidParamException("inset-separator",
+						((new Character(op)).toString()), ac);
+			}
+			switch (val.getType()) {
+				case CssTypes.CSS_LENGTH:
+					break;
+				case CssTypes.CSS_PERCENTAGE:
+					break;
+				default:
+					throw new InvalidParamException("value",
+							val.toString(),
+							caller.getPropertyName(), ac);
+			}
+			expression.next();
+		}
+	}
 }
 

org/w3c/css/util/Messages.properties.en
@@ -232,6 +232,9 @@ error.shape: Invalid shape definition rect(<top>,<right>,<bottom>,<left>)
 error.shape-separator: Invalid separator in shape definition. It must be a comma.
 warning.shape-separator: Invalid separator in shape definition. It must be a comma.
 
+# used by org.w3c.svg.properties.CssClipPath
+error.inset-separator: Invalid separator \u201C%s\u201D in \u201Cinset\u201D definition (must be a space)
+
 # used by org.w3c.css.properties.CssContent
 error.attr: Invalid attr definition attr(X)
 error.function: Invalid function definition 

commit 01e5d08619b89780472e72f2a96feb1cb58f1bdd

remove the inset-separator error introduced in 66c715722083bef5db4dcc5ea721dbb967278412

org/w3c/css/properties/svg/CssClipPath.java
@@ -219,7 +219,7 @@ public class CssClipPath extends org.w3c.css.properties.css.CssClipPath {
                             caller.getPropertyName(), ac);
             }
             if (op != SPACE) {
-                throw new InvalidParamException("inset-separator",
+                throw new InvalidParamException("operator",
                         ((new Character(op)).toString()), ac);
             }
             expression.next();

org/w3c/css/util/Messages.properties.en
@@ -232,9 +232,6 @@ error.shape: Invalid shape definition rect(<top>,<right>,<bottom>,<left>)
 error.shape-separator: Invalid separator in shape definition. It must be a comma.
 warning.shape-separator: Invalid separator in shape definition. It must be a comma.
 
-# used by org.w3c.svg.properties.CssClipPath
-error.inset-separator: Invalid separator \u201C%s\u201D in \u201Cinset\u201D definition (must be a space)
-
 # used by org.w3c.css.properties.CssContent
 error.attr: Invalid attr definition attr(X)
 error.function: Invalid function definition 