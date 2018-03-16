24aefb540cd1bcf9412988d23105709afbdd765a
Yves Lafon <ylafon@w3.org>
2018-03-16 18:57:31 +0100

    aligned with https://www.w3.org/TR/2017/WD-css-text-3-20170822/#overflow-wrap-property

--- org/w3c/css/properties/css3/CssOverflowWrap.java
+++ org/w3c/css/properties/css3/CssOverflowWrap.java
@@ -1,4 +1,4 @@
-// $Id$
+//
 // Author: Yves Lafon <ylafon@w3.org>
 //
 // (c) COPYRIGHT MIT, ERCIM and Keio University, 2012.
@@ -11,19 +11,25 @@ import org.w3c.css.values.CssExpression;
 import org.w3c.css.values.CssIdent;
 import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
+import org.w3c.css.values.CssValueList;
+
+import java.util.ArrayList;
+
+import static org.w3c.css.values.CssOperator.SPACE;
 
 /**
- * @spec http://www.w3.org/TR/2012/WD-css3-text-20120814/#overflow-wrap0
+ * @spec hhttps://www.w3.org/TR/2017/WD-css-text-3-20170822/#overflow-wrap-property
  * <p/>
  * Note that word-wrap is also an alias for this.
  */
 public class CssOverflowWrap extends org.w3c.css.properties.css.CssOverflowWrap {
 
-    public static final CssIdent normal, break_word;
+    public static final CssIdent normal, break_word, break_spaces;
 
     static {
         normal = CssIdent.getIdent("normal");
         break_word = CssIdent.getIdent("break-word");
+        break_spaces = CssIdent.getIdent("break-spaces");
     }
 
     /**
@@ -42,7 +48,7 @@ public class CssOverflowWrap extends org.w3c.css.properties.css.CssOverflowWrap 
      */
     public CssOverflowWrap(ApplContext ac, CssExpression expression, boolean check)
             throws InvalidParamException {
-        if (check && expression.getCount() > 1) {
+        if (check && expression.getCount() > 2) {
             throw new InvalidParamException("unrecognize", ac);
         }
         setByUser();
@@ -50,29 +56,60 @@ public class CssOverflowWrap extends org.w3c.css.properties.css.CssOverflowWrap 
         CssValue val;
         char op;
 
-        val = expression.getValue();
-        op = expression.getOperator();
+        ArrayList<CssValue> values = new ArrayList<>();
+        boolean gotNormal = false;
+        boolean gotBw = false;
+        boolean gotBs = false;
 
-        if (val.getType() == CssTypes.CSS_IDENT) {
-            CssIdent ident = (CssIdent) val;
-            if (inherit.equals(ident)) {
-                value = inherit;
-            } else if (normal.equals(ident)) {
-                value = normal;
-            } else if (break_word.equals(ident)) {
-                value = break_word;
+        while (!expression.end()) {
+            val = expression.getValue();
+            op = expression.getOperator();
+
+            if (val.getType() == CssTypes.CSS_IDENT) {
+                CssIdent ident = (CssIdent) val;
+                if (inherit.equals(ident)) {
+                    if (expression.getCount() > 1) {
+                        throw new InvalidParamException("unrecognize", ac);
+                    }
+                    value = inherit;
+                } else if (normal.equals(ident)) {
+                    if (gotNormal || gotBw || gotBs) {
+                        throw new InvalidParamException("unrecognize", ac);
+                    }
+                    value = normal;
+                    gotNormal = true;
+                } else if (break_word.equals(ident)) {
+                    if (gotNormal || gotBw) {
+                        throw new InvalidParamException("unrecognize", ac);
+                    }
+                    values.add(break_word);
+                    gotBw = true;
+                } else if (break_spaces.equals(ident)) {
+                    if (gotNormal || gotBs) {
+                        throw new InvalidParamException("unrecognize", ac);
+                    }
+                    values.add(break_spaces);
+                    gotBs = true;
+                } else {
+                    throw new InvalidParamException("value",
+                            val.toString(),
+                            getPropertyName(), ac);
+                }
             } else {
                 throw new InvalidParamException("value",
                         val.toString(),
                         getPropertyName(), ac);
             }
-        } else {
-            throw new InvalidParamException("value",
-                    val.toString(),
-                    getPropertyName(), ac);
+            if (op != SPACE) {
+                throw new InvalidParamException("operator",
+                        ((new Character(op)).toString()),
+                        ac);
+            }
+            expression.next();
         }
-        expression.next();
-
+        if (values.size() > 0) {
+            value = values.size() == 1 ? values.get(0) : new CssValueList(values);
+        }
     }
 
     public CssOverflowWrap(ApplContext ac, CssExpression expression)
