2b710718a80f6409a642eda51c95352d261cde52
Yves Lafon <ylafon@w3.org>
2018-03-16 19:03:49 +0100

    aligned with https://www.w3.org/TR/2017/WD-css-text-3-20170822/#letter-spacing-property

--- org/w3c/css/properties/css3/CssLetterSpacing.java
+++ org/w3c/css/properties/css3/CssLetterSpacing.java
@@ -1,4 +1,5 @@
-// $Id$
+//
+// Author: Yves Lafon <ylafon@w3.org>
 //
 // (c) COPYRIGHT MIT, INRIA and Keio University, 2011
 // Please first read the full copyright statement in file COPYRIGHT.html
@@ -8,15 +9,11 @@ import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 import org.w3c.css.values.CssExpression;
 import org.w3c.css.values.CssIdent;
-import org.w3c.css.values.CssOperator;
 import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
-import org.w3c.css.values.CssValueList;
-
-import java.util.ArrayList;
 
 /**
- * @spec http://www.w3.org/TR/2011/WD-css3-text-20110901/#letter-spacing0
+ * @spec https://www.w3.org/TR/2017/WD-css-text-3-20170822/#letter-spacing-property
  */
 public class CssLetterSpacing extends org.w3c.css.properties.css.CssLetterSpacing {
 
@@ -38,51 +35,34 @@ public class CssLetterSpacing extends org.w3c.css.properties.css.CssLetterSpacin
      */
     public CssLetterSpacing(ApplContext ac, CssExpression expression,
                             boolean check) throws InvalidParamException {
-        if (check && expression.getCount() > 3) {
+        if (check && expression.getCount() > 1) {
             throw new InvalidParamException("unrecognize", ac);
         }
         setByUser();
 
-        ArrayList<CssValue> v = new ArrayList<CssValue>(4);
-        CssValue val;
-        char op;
 
-        while (!expression.end()) {
-            val = expression.getValue();
-            op = expression.getOperator();
+        CssValue val = expression.getValue();
 
-            switch (val.getType()) {
-                case CssTypes.CSS_NUMBER:
-                    val.getCheckableValue().checkEqualsZero(ac, this);
-                case CssTypes.CSS_LENGTH:
-                    v.add(val);
+        switch (val.getType()) {
+            case CssTypes.CSS_NUMBER:
+                val.getCheckableValue().checkEqualsZero(ac, this);
+            case CssTypes.CSS_LENGTH:
+                value = val;
+                break;
+            case CssTypes.CSS_IDENT:
+                if (inherit.equals(val)) {
+                    value = inherit;
                     break;
-                case CssTypes.CSS_IDENT:
-                    if (inherit.equals(val)) {
-                        // inherit can only be alone
-                        if (expression.getCount() > 1) {
-                            throw new InvalidParamException("value", expression.getValue(),
-                                    getPropertyName(), ac);
-                        }
-                        value = inherit;
-                        break;
-                    } else if (normal.equals(val)) {
-                        v.add(normal);
-                        break;
-                    }
-                default:
-                    throw new InvalidParamException("value", expression.getValue(),
-                            getPropertyName(), ac);
-            }
-            if (op != CssOperator.SPACE) {
-                throw new InvalidParamException("operator",
-                        ((new Character(op)).toString()), ac);
-            }
-            expression.next();
+                } else if (normal.equals(val)) {
+                    value = normal;
+                    break;
+                }
+            default:
+                throw new InvalidParamException("value", expression.getValue(),
+                        getPropertyName(), ac);
         }
-        if (value != inherit) {
-            value = (v.size() == 1) ? v.get(0) : new CssValueList(v);
-        }
+        expression.next();
+
     }
 
     public CssLetterSpacing(ApplContext ac, CssExpression expression)
