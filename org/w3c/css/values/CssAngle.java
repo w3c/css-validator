w3c/css-validator

commit 1af8a7788f011289a636e96fc34c788a3bc9d553

allow other objects than properteis to emit errors and warnings for values (media features, for instance)

org/w3c/css/values/CssAngle.java
@@ -6,7 +6,6 @@
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.values;
 
-import org.w3c.css.properties.css.CssProperty;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 
@@ -202,74 +201,28 @@ public class CssAngle extends CssCheckableValue implements CssValueFloat {
         return BigDecimal.ZERO.equals(normalize(value));
     }
 
-    /**
-     * check if the value is positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isPositive()) {
-            throw new InvalidParamException("negative-value",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * check if the value is strictly positive
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkStrictPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isStrictlyPositive()) {
-            throw new InvalidParamException("strictly-positive",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * warn if the value is not positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     */
-    public void warnPositiveness(ApplContext ac, CssProperty property) {
-        if (!isPositive()) {
-            ac.getFrame().addWarning("negative", toString());
-        }
-    }
 
     /**
      * check if the value is equal to zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      * @throws InvalidParamException
      */
-    public void checkEqualsZero(ApplContext ac, CssProperty property)
+    public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        if (!isZero()) {
-            throw new InvalidParamException("zero",
-                    toString(),
-                    "angle", ac);
-        }
+        checkEqualsZero(ac, new String[]{toString(), "angle", callername});
     }
 
     /**
      * warn if the value is not zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      */
-    public void warnEqualsZero(ApplContext ac, CssProperty property) {
-        if (!isZero()) {
-            ac.getFrame().addWarning("zero", "angle");
-        }
+    public void warnEqualsZero(ApplContext ac, String callername) {
+        warnEqualsZero(ac, new String[]{"angle", callername});
     }
+
 }
 

org/w3c/css/values/CssFlexibleLength.java
@@ -5,7 +5,6 @@
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.values;
 
-import org.w3c.css.properties.css.CssProperty;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 
@@ -160,78 +159,29 @@ public class CssFlexibleLength extends CssCheckableValue {
                 unit.equals(((CssFlexibleLength) value).unit));
     }
 
-
-    /**
-     * check if the value is positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws org.w3c.css.util.InvalidParamException
-     *
-     */
-    public void checkPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isPositive()) {
-            throw new InvalidParamException("negative-value",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * check if the value is strictly positive
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws org.w3c.css.util.InvalidParamException
-     *
-     */
-    public void checkStrictPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isStrictlyPositive()) {
-            throw new InvalidParamException("strictly-positive",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * warn if the value is not positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     */
-    public void warnPositiveness(ApplContext ac, CssProperty property) {
-        if (!isPositive()) {
-            ac.getFrame().addWarning("negative", toString());
-        }
-    }
+    // redefinitions for some error messages.
 
     /**
      * check if the value is equal to zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws org.w3c.css.util.InvalidParamException
-     *
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
+     * @throws InvalidParamException
      */
-    public void checkEqualsZero(ApplContext ac, CssProperty property)
+    public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        if (!isZero()) {
-            throw new InvalidParamException("zero",
-                    toString(),
-                    "length", ac);
-        }
+        checkEqualsZero(ac, new String[]{toString(), "length", callername});
     }
 
     /**
      * warn if the value is not zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      */
-    public void warnEqualsZero(ApplContext ac, CssProperty property) {
-        if (!isZero()) {
-            ac.getFrame().addWarning("zero", "length");
-        }
+    public void warnEqualsZero(ApplContext ac, String callername) {
+        warnEqualsZero(ac, new String[]{"length", callername});
     }
+
 }
 

org/w3c/css/values/CssFrequency.java
@@ -6,7 +6,6 @@
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.values;
 
-import org.w3c.css.properties.css.CssProperty;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 
@@ -174,78 +173,35 @@ public class CssFrequency extends CssCheckableValue {
         return (BigDecimal.ZERO.compareTo(value) == 0);
     }
 
-    /**
-     * check if the value is positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isPositive()) {
-            throw new InvalidParamException("negative-value",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * check if the value is strictly positive
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkStrictPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isStrictlyPositive()) {
-            throw new InvalidParamException("strictly-positive",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * warn if the value is not positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     */
-    public void warnPositiveness(ApplContext ac, CssProperty property) {
-        if (!isPositive()) {
-            ac.getFrame().addWarning("negative", toString());
-        }
-    }
 
     public CssFrequency getFrequency() {
         return this;
     }
 
+
+    // redefinitions for some error messages.
+
     /**
      * check if the value is equal to zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      * @throws InvalidParamException
      */
-    public void checkEqualsZero(ApplContext ac, CssProperty property)
+    public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        if (!isZero()) {
-            throw new InvalidParamException("zero",
-                    toString(),
-                    "frequency", ac);
-        }
+        checkEqualsZero(ac, new String[]{toString(), "frequency", callername});
     }
 
     /**
      * warn if the value is not zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      */
-    public void warnEqualsZero(ApplContext ac, CssProperty property) {
-        if (!isZero()) {
-            ac.getFrame().addWarning("zero", "frequency");
-        }
+    public void warnEqualsZero(ApplContext ac, String callername) {
+        warnEqualsZero(ac, new String[]{"frequency", callername});
     }
+
 }
 

org/w3c/css/values/CssLength.java
@@ -7,7 +7,6 @@
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.values;
 
-import org.w3c.css.properties.css.CssProperty;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 
@@ -259,75 +258,29 @@ public class CssLength extends CssCheckableValue {
                 unit.equals(((CssLength) value).unit));
     }
 
-
-    /**
-     * check if the value is positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isPositive()) {
-            throw new InvalidParamException("negative-value",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * check if the value is strictly positive
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkStrictPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isStrictlyPositive()) {
-            throw new InvalidParamException("strictly-positive",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * warn if the value is not positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     */
-    public void warnPositiveness(ApplContext ac, CssProperty property) {
-        if (!isPositive()) {
-            ac.getFrame().addWarning("negative", toString());
-        }
-    }
+    // redefinitions for some error messages.
 
     /**
      * check if the value is equal to zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      * @throws InvalidParamException
      */
-    public void checkEqualsZero(ApplContext ac, CssProperty property)
+    public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        if (!isZero()) {
-            throw new InvalidParamException("zero",
-                    toString(),
-                    "length", ac);
-        }
+        checkEqualsZero(ac, new String[]{toString(), "length", callername});
     }
 
     /**
      * warn if the value is not zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      */
-    public void warnEqualsZero(ApplContext ac, CssProperty property) {
-        if (!isZero()) {
-            ac.getFrame().addWarning("zero", "length");
-        }
+    public void warnEqualsZero(ApplContext ac, String callername) {
+        warnEqualsZero(ac, new String[]{"length", callername});
     }
+
 }
 

org/w3c/css/values/CssNumber.java
@@ -244,63 +244,6 @@ public class CssNumber extends CssCheckableValue implements CssValueFloat {
     }
 
     /**
-     * check if the value is positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isPositive()) {
-            throw new InvalidParamException("negative-value",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * check if the value is strictly positive
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkStrictPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isStrictlyPositive()) {
-            throw new InvalidParamException("strictly-positive",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * check if the value is an integer
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkInteger(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isInteger()) {
-            throw new InvalidParamException("integer",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * warn if the value is not positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     */
-    public void warnPositiveness(ApplContext ac, CssProperty property) {
-        if (!isPositive()) {
-            ac.getFrame().addWarning("negative", toString());
-        }
-    }
-
-    /**
      * check if the value is lower or equal than...
      *
      * @param ac       the validation context
@@ -370,27 +313,22 @@ public class CssNumber extends CssCheckableValue implements CssValueFloat {
     /**
      * check if the value is equal to zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      * @throws InvalidParamException
      */
-    public void checkEqualsZero(ApplContext ac, CssProperty property)
+    public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        if (!isZero()) {
-            throw new InvalidParamException("zero",
-                    "unit", toString(), ac);
-        }
+        checkEqualsZero(ac, new String[]{toString(), "unit", callername});
     }
 
     /**
      * warn if the value is not zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      */
-    public void warnEqualsZero(ApplContext ac, CssProperty property) {
-        if (!isZero()) {
-            ac.getFrame().addWarning("zero", "unit");
-        }
+    public void warnEqualsZero(ApplContext ac, String callername) {
+        warnEqualsZero(ac, new String[]{"unit", callername});
     }
 }

org/w3c/css/values/CssPercentage.java
@@ -177,48 +177,6 @@ public class CssPercentage extends CssCheckableValue {
     }
 
     /**
-     * check if the value is positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isPositive()) {
-            throw new InvalidParamException("negative-value",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * check if the value is strictly positive
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws InvalidParamException
-     */
-    public void checkStrictPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isStrictlyPositive()) {
-            throw new InvalidParamException("strictly-positive",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * warn if the value is not positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     */
-    public void warnPositiveness(ApplContext ac, CssProperty property) {
-        if (!isPositive()) {
-            ac.getFrame().addWarning("negative", toString());
-        }
-    }
-
-    /**
      * check if the value is lower or equal than...
      *
      * @param ac       the validation context
@@ -254,28 +212,23 @@ public class CssPercentage extends CssCheckableValue {
     /**
      * check if the value is equal to zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      * @throws InvalidParamException
      */
-    public void checkEqualsZero(ApplContext ac, CssProperty property)
+    public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        if (!isZero()) {
-            throw new InvalidParamException("zero",
-                    toString(),
-                    "percentage", ac);
-        }
+        checkEqualsZero(ac, new String[]{toString(), "percentage", callername});
     }
 
     /**
      * warn if the value is not zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      */
-    public void warnEqualsZero(ApplContext ac, CssProperty property) {
-        if (!isZero()) {
-            ac.getFrame().addWarning("zero", "percentage");
-        }
+    public void warnEqualsZero(ApplContext ac, String callername) {
+        warnEqualsZero(ac, new String[]{"percentage", callername});
     }
+
 }

org/w3c/css/values/CssSemitone.java
@@ -5,7 +5,6 @@
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.values;
 
-import org.w3c.css.properties.css.CssProperty;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 
@@ -157,77 +156,26 @@ public class CssSemitone extends CssCheckableValue {
                 unit.equals(((CssSemitone) value).unit));
     }
 
-
-    /**
-     * check if the value is positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws org.w3c.css.util.InvalidParamException
-     *
-     */
-    public void checkPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isPositive()) {
-            throw new InvalidParamException("negative-value",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * check if the value is strictly positive
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     * @throws org.w3c.css.util.InvalidParamException
-     *
-     */
-    public void checkStrictPositiveness(ApplContext ac, CssProperty property)
-            throws InvalidParamException {
-        if (!isStrictlyPositive()) {
-            throw new InvalidParamException("strictly-positive",
-                    toString(), property.getPropertyName(), ac);
-        }
-    }
-
-    /**
-     * warn if the value is not positive or null
-     *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
-     */
-    public void warnPositiveness(ApplContext ac, CssProperty property) {
-        if (!isPositive()) {
-            ac.getFrame().addWarning("negative", toString());
-        }
-    }
-
     /**
      * check if the value is equal to zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      * @throws InvalidParamException
      */
-    public void checkEqualsZero(ApplContext ac, CssProperty property)
+    public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        if (!isZero()) {
-            throw new InvalidParamException("zero",
-                    toString(),
-                    "semitone", ac);
-        }
+        checkEqualsZero(ac, new String[]{toString(), "semitone", callername});
     }
 
     /**
      * warn if the value is not zero
      *
-     * @param ac       the validation context
-     * @param property the property the value is defined in
+     * @param ac         the validation context
+     * @param callername the String value of the object it is defined in
      */
-    public void warnEqualsZero(ApplContext ac, CssProperty property) {
-        if (!isZero()) {
-            ac.getFrame().addWarning("zero", "semitone");
-        }
+    public void warnEqualsZero(ApplContext ac, String callername) {
+        warnEqualsZero(ac, new String[]{"semitone", callername});
     }
 }
 

commit f8713cb581d3a65887551421364f398ed04c3e10

fixed improper ordering of parameters for the 'zero' error introduced in 1af8a7788f011289a636e96fc34c788a3bc9d553

org/w3c/css/values/CssAngle.java
@@ -211,7 +211,7 @@ public class CssAngle extends CssCheckableValue implements CssValueFloat {
      */
     public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        checkEqualsZero(ac, new String[]{toString(), "angle", callername});
+        checkEqualsZero(ac, new String[]{"angle", toString(), callername});
     }
 
     /**

org/w3c/css/values/CssFlexibleLength.java
@@ -170,7 +170,7 @@ public class CssFlexibleLength extends CssCheckableValue {
      */
     public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        checkEqualsZero(ac, new String[]{toString(), "length", callername});
+        checkEqualsZero(ac, new String[]{"length", toString(), callername});
     }
 
     /**

org/w3c/css/values/CssFrequency.java
@@ -190,7 +190,7 @@ public class CssFrequency extends CssCheckableValue {
      */
     public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        checkEqualsZero(ac, new String[]{toString(), "frequency", callername});
+        checkEqualsZero(ac, new String[]{"frequency", toString(), callername});
     }
 
     /**

org/w3c/css/values/CssLength.java
@@ -269,7 +269,7 @@ public class CssLength extends CssCheckableValue {
      */
     public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        checkEqualsZero(ac, new String[]{toString(), "length", callername});
+        checkEqualsZero(ac, new String[]{"length", toString(), callername});
     }
 
     /**

org/w3c/css/values/CssNumber.java
@@ -319,7 +319,7 @@ public class CssNumber extends CssCheckableValue implements CssValueFloat {
      */
     public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        checkEqualsZero(ac, new String[]{toString(), "unit", callername});
+        checkEqualsZero(ac, new String[]{"unit", toString(), callername});
     }
 
     /**
@@ -343,7 +343,7 @@ public class CssNumber extends CssCheckableValue implements CssValueFloat {
             throws InvalidParamException {
         if (!isInteger()) {
             throw new InvalidParamException("integer",
-                    toString(), callername, ac);
+                    new String[]{toString(), callername}, ac);
         }
     }
 }

org/w3c/css/values/CssPercentage.java
@@ -218,7 +218,7 @@ public class CssPercentage extends CssCheckableValue {
      */
     public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        checkEqualsZero(ac, new String[]{toString(), "percentage", callername});
+        checkEqualsZero(ac, new String[]{"percentage", toString(), callername});
     }
 
     /**

org/w3c/css/values/CssSemitone.java
@@ -165,7 +165,7 @@ public class CssSemitone extends CssCheckableValue {
      */
     public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        checkEqualsZero(ac, new String[]{toString(), "semitone", callername});
+        checkEqualsZero(ac, new String[]{"semitone", toString(), callername});
     }
 
     /**

org/w3c/css/values/CssTime.java
@@ -183,7 +183,7 @@ public class CssTime extends CssCheckableValue {
      */
     public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        checkEqualsZero(ac, new String[]{toString(), "time", callername});
+        checkEqualsZero(ac, new String[]{"time", toString(), callername});
     }
 
     /**

org/w3c/css/values/CssVolume.java
@@ -165,7 +165,7 @@ public class CssVolume extends CssCheckableValue {
      */
     public void checkEqualsZero(ApplContext ac, String callername)
             throws InvalidParamException {
-        checkEqualsZero(ac, new String[]{toString(), "volume", callername});
+        checkEqualsZero(ac, new String[]{"volume", toString(), callername});
     }
 
     /**