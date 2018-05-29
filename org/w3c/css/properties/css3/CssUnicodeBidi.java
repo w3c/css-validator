b6a093b97052b47b625d5d3eec082b42010bd330
Yves Lafon <ylafon@w3.org>
2018-05-29 13:38:16 +0200

    fix spec link (values unchanged) per https://www.w3.org/TR/2018/CR-css-writing-modes-3-20180524/#propdef-unicode-bidi

--- org/w3c/css/properties/css3/CssUnicodeBidi.java
+++ org/w3c/css/properties/css3/CssUnicodeBidi.java
@@ -13,7 +13,7 @@ import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
 
 /**
- * @spec http://www.w3.org/TR/2012/WD-css3-writing-modes-20121115/#unicode-bidi
+ * @spec https://www.w3.org/TR/2018/CR-css-writing-modes-3-20180524/#propdef-unicode-bidi
  */
 public class CssUnicodeBidi extends org.w3c.css.properties.css.CssUnicodeBidi {
 
