w3c/css-validator

commit 3fcc6d846725adc9988ca60ed8b662ca722ff03a

default to english version when a message is not found in a particular language This addresses #190

org/w3c/css/util/Messages.java
@@ -24,6 +24,7 @@ public class Messages {
 
     public static final ConcurrentHashMap<String, Utf8Properties<String, String>> languages;
     public static final ArrayList<String> languages_name;
+    public Utf8Properties<String, String> default_properties = null;
 
     /**
      * Creates a new Messages
@@ -80,8 +81,9 @@ public class Messages {
                 }
             }
         }
+        default_properties = languages.get("en");
         if (properties == null) {
-            properties = languages.get("en");
+            properties = default_properties;
         }
     }
 
@@ -89,7 +91,11 @@ public class Messages {
      * Get a property.
      */
     public String getString(String message) {
-        return properties.getProperty(message);
+        String s = properties.getProperty(message);
+        if (s != null) {
+            return s;
+        }
+        return default_properties.getProperty(message);
     }
 
     /**

commit 146bacf32ebd1a58a8faae24a4d8aee6c38aac02

Add a way to get messages without defaulting for the translation page, this fix the regression introduced in 3fcc6d846725adc9988ca60ed8b662ca722ff03a

org/w3c/css/index/TranslationTableGenerator.java
@@ -149,7 +149,7 @@ public class TranslationTableGenerator {
                 for (int i = 0; i < Messages.languages_name.size(); ++i) {
                     HashMap<String, String> language = languages.get(i);
                     ApplContext translation = translations.get(language.get("name"));
-                    String property_translated = translation.getMsg().getString(property_name);
+                    String property_translated = translation.getMsg().getStringStrict(property_name);
                     if (language.get("name").equals(default_lang)) {
                         vc.put(property_name, property_translated);
                     } else {

org/w3c/css/util/Messages.java
@@ -99,6 +99,13 @@ public class Messages {
     }
 
     /**
+     * Get a property but not its default when not found
+     */
+    public String getStringStrict(String message) {
+        return properties.getProperty(message);
+    }
+
+    /**
      * Get a warning property.
      *
      * @param message the warning property.