b8c583804ae57b4d16d1fae952ec255384016eda
Michael[tm] Smith <mike@w3.org>
2019-04-19 21:14:27 +0900

    Remove setContentEncoding from ApplContext
    The setContentEncoding method of the ApplContext class isn’t used in the
    HTML checker context, so this change completely removes the method. In
    combination with other changes that eliminated dependencies, this change
    eliminates any remaining dependency on Jigsaw.

--- org/w3c/css/util/ApplContext.java
+++ org/w3c/css/util/ApplContext.java
@@ -10,9 +10,6 @@ package org.w3c.css.util;
 
 
 import org.w3c.css.parser.Frame;
-import org.w3c.www.http.HttpAcceptCharset;
-import org.w3c.www.http.HttpAcceptCharsetList;
-import org.w3c.www.http.HttpFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -337,64 +334,6 @@ public class ApplContext {
 
     public void setSuggestPropertyName(boolean b) {
         suggestPropertyName = b;
-    }
-
-    /**
-     * Sets the content encoding to the first charset that appears in
-     * <i>acceptCharset</i>. If the charset is not supported, the content
-     * encoding will be utf-8
-     *
-     * @param acceptCharset a String representing the Accept-Charset request parameter
-     */
-    public void setContentEncoding(String acceptCharset) {
-        if (acceptCharset != null) {
-            // uses some Jigsaw classes to parse the Accept-Charset
-            // these classes need to load a lot of stuff, so it may be quite
-            // long the first time
-            HttpAcceptCharsetList charsetList;
-            HttpAcceptCharset[] charsets;
-
-            charsetList = HttpFactory.parseAcceptCharsetList(acceptCharset);
-            charsets = (HttpAcceptCharset[]) charsetList.getValue();
-
-            String encoding = null;
-            double quality = 0.0;
-
-            String biasedcharset = getMsg().getString("output-encoding-name");
-
-            for (int i = 0; i < charsets.length && quality < 1.0; i++) {
-                HttpAcceptCharset charset = charsets[i];
-
-                String currentCharset = charset.getCharset();
-
-                // checks that the charset is supported by Java
-
-                if (isCharsetSupported(currentCharset)) {
-                    double currentQuality = charset.getQuality();
-
-                    // we prefer utf-8
-                    // FIXME (the bias value and the biased charset
-                    // should be dependant on the language)
-                    if ((biasedcharset != null) &&
-                            !biasedcharset.equalsIgnoreCase(currentCharset)) {
-                        currentQuality *= 0.5;
-                    }
-                    if (currentQuality > quality) {
-                        quality = currentQuality;
-                        encoding = charset.getCharset();
-                    }
-                }
-            }
-            if (encoding != null) {
-                getMsg().properties.setProperty("content-encoding", encoding);
-            } else {
-                // no valid charset
-                getMsg().properties.remove("content-encoding");
-            }
-        } else {
-            // no Accept-Charset given
-            getMsg().properties.remove("content-encoding");
-        }
     }
 
     private boolean isCharsetSupported(String charset) {
