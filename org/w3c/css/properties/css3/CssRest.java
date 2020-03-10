5b301af45735dd364d6f3247d6124f198f9f9a97
Yves Lafon <ylafon@w3.org>
2020-03-10 10:30:31 +0100

    refactored rest check, this follows https://www.w3.org/TR/2020/CR-css-speech-1-20200310/#rest and avoids code duplication

--- org/w3c/css/properties/css3/CssRest.java
+++ org/w3c/css/properties/css3/CssRest.java
@@ -1,23 +1,47 @@
-// $Id$
+//
 // Author: Yves Lafon <ylafon@w3.org>
 //
-// (c) COPYRIGHT MIT, ERCIM and Keio University, 2013.
+// (c) COPYRIGHT MIT, ERCIM, Keio, Beihang, 2013.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.properties.css3;
 
+import org.w3c.css.properties.css.CssProperty;
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
+import org.w3c.css.values.CssCheckableValue;
 import org.w3c.css.values.CssExpression;
+import org.w3c.css.values.CssIdent;
 import org.w3c.css.values.CssOperator;
+import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
 import org.w3c.css.values.CssValueList;
 
 import java.util.ArrayList;
 
 /**
- * @spec http://www.w3.org/TR/2012/CR-css3-speech-20120320/#rest
+ * @spec https://www.w3.org/TR/2020/CR-css-speech-1-20200310/#rest
  */
 public class CssRest extends org.w3c.css.properties.css.CssRest {
+
+    public static final CssIdent[] allowed_values;
+
+    static {
+        String[] _allowed_values = {"none", "x-weak", "weak", "medium", "strong", "x-strong"};
+        int i = 0;
+        allowed_values = new CssIdent[_allowed_values.length];
+        for (String s : _allowed_values) {
+            allowed_values[i++] = CssIdent.getIdent(s);
+        }
+    }
+
+    public static final CssIdent getAllowedIdent(CssIdent ident) {
+        for (CssIdent id : allowed_values) {
+            if (id.equals(ident)) {
+                return id;
+            }
+        }
+        return null;
+    }
 
     /**
      * Create a new CssRest
@@ -44,7 +68,8 @@ public class CssRest extends org.w3c.css.properties.css.CssRest {
 
         char op;
 
-        cssRestBefore = new CssRestBefore(ac, expression, false);
+        cssRestBefore = new CssRestBefore();
+        cssRestBefore.value = checkRestValue(ac, expression, this);
         if (expression.end()) {
             cssRestAfter = new CssRestAfter();
             cssRestAfter.value = cssRestBefore.value;
@@ -55,11 +80,12 @@ public class CssRest extends org.w3c.css.properties.css.CssRest {
                 throw new InvalidParamException("operator",
                         ((new Character(op)).toString()), ac);
             }
-            cssRestAfter = new CssRestAfter(ac, expression, false);
             if (cssRestBefore.value == inherit || cssRestAfter.value == inherit) {
                 throw new InvalidParamException("value",
                         inherit, getPropertyName(), ac);
             }
+            cssRestAfter = new CssRestAfter();
+            cssRestAfter.value = checkRestValue(ac, expression, this);
             ArrayList<CssValue> values = new ArrayList<CssValue>(2);
             values.add(cssRestBefore.value);
             values.add(cssRestAfter.value);
@@ -70,6 +96,43 @@ public class CssRest extends org.w3c.css.properties.css.CssRest {
     public CssRest(ApplContext ac, CssExpression expression)
             throws InvalidParamException {
         this(ac, expression, false);
+    }
+
+    protected static CssValue checkRestValue(ApplContext ac, CssExpression expression,
+                                              CssProperty caller)
+            throws InvalidParamException {
+        CssValue val, v;
+        char op;
+
+        val = expression.getValue();
+        op = expression.getOperator();
+
+        switch (val.getType()) {
+            case CssTypes.CSS_NUMBER:
+                val.getCheckableValue().checkEqualsZero(ac, caller);
+                expression.next();
+                return (val);
+            case CssTypes.CSS_TIME:
+                CssCheckableValue t = val.getCheckableValue();
+                t.checkPositiveness(ac, caller);
+                expression.next();
+                return val;
+            case CssTypes.CSS_IDENT:
+                CssIdent id = (CssIdent) val;
+                if (inherit.equals(id)) {
+                    expression.next();
+                    return inherit;
+                }
+                v = getAllowedIdent(id);
+                if (v != null) {
+                    expression.next();
+                    return v;
+                }
+            default:
+                throw new InvalidParamException("value",
+                        val.toString(),
+                        caller.getPropertyName(), ac);
+        }
     }
 }
 
--- org/w3c/css/properties/css3/CssRestBefore.java
+++ org/w3c/css/properties/css3/CssRestBefore.java
@@ -1,43 +1,18 @@
-// $Id$
+//
 // Author: Yves Lafon <ylafon@w3.org>
 //
-// (c) COPYRIGHT MIT, ERCIM and Keio University, 2013.
+// (c) COPYRIGHT MIT, ERCIM, Keio, Beihang, 2013.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.properties.css3;
 
 import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
-import org.w3c.css.values.CssCheckableValue;
 import org.w3c.css.values.CssExpression;
-import org.w3c.css.values.CssIdent;
-import org.w3c.css.values.CssTypes;
-import org.w3c.css.values.CssValue;
 
 /**
- * @spec http://www.w3.org/TR/2012/CR-css3-speech-20120320/#rest-before
+ * @spec https://www.w3.org/TR/2020/CR-css-speech-1-20200310/#rest-before
  */
 public class CssRestBefore extends org.w3c.css.properties.css.CssRestBefore {
-
-
-    public static final CssIdent[] allowed_values;
-
-    static {
-        String[] _allowed_values = {"none", "x-weak", "weak", "medium", "strong", "x-strong"};
-        int i = 0;
-        allowed_values = new CssIdent[_allowed_values.length];
-        for (String s : _allowed_values) {
-            allowed_values[i++] = CssIdent.getIdent(s);
-        }
-    }
-
-    public static final CssIdent getAllowedIdent(CssIdent ident) {
-        for (CssIdent id : allowed_values) {
-            if (id.equals(ident)) {
-                return id;
-            }
-        }
-        return null;
-    }
 
     /**
      * Create a new CssRestBefore
@@ -60,38 +35,7 @@ public class CssRestBefore extends org.w3c.css.properties.css.CssRestBefore {
         }
         setByUser();
 
-        CssValue val;
-        char op;
-
-        val = expression.getValue();
-        op = expression.getOperator();
-
-        switch (val.getType()) {
-            case CssTypes.CSS_NUMBER:
-                val.getCheckableValue().checkEqualsZero(ac, this);
-                value = val;
-                break;
-            case CssTypes.CSS_TIME:
-                CssCheckableValue t = val.getCheckableValue();
-                t.checkPositiveness(ac, this);
-                value = val;
-                break;
-            case CssTypes.CSS_IDENT:
-                CssIdent id = (CssIdent) val;
-                if (inherit.equals(id)) {
-                    value = inherit;
-                    break;
-                }
-                value = getAllowedIdent(id);
-                if (value != null) {
-                    break;
-                }
-            default:
-                throw new InvalidParamException("value",
-                        val.toString(),
-                        getPropertyName(), ac);
-        }
-        expression.next();
+        value = CssRest.checkRestValue(ac, expression, this);
     }
 
     public CssRestBefore(ApplContext ac, CssExpression expression)
