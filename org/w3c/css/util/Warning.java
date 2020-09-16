213e2ea091a5a3fc943f9d7c1ac070e79bcfdd74
Yves Lafon <ylafon@w3.org>
2020-09-17 00:01:31 +0200

    same as earlier cache for Errors, but now for Warnings

--- org/w3c/css/util/Warning.java
+++ org/w3c/css/util/Warning.java
@@ -9,6 +9,7 @@ package org.w3c.css.util;
 import org.w3c.css.parser.CssSelectors;
 import org.w3c.css.properties.css.CssProperty;
 
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -26,6 +27,15 @@ public class Warning implements Comparable<Warning> {
     CssSelectors selector;
     String warningMessage;
     String warningType = null;
+
+    private static HashMap<String, String[]> warningMessages;
+    private static HashMap<String, int[]> warningOrder;
+
+    static {
+        warningMessages = new HashMap<>();
+        warningOrder = new HashMap<>();
+    }
+
 
     /**
      * Create a new Warning with message parameters.
@@ -214,72 +224,85 @@ public class Warning implements Comparable<Warning> {
         // replace all parameters.
         if (args != null) {
             StringBuilder sb = new StringBuilder();
-            int idx = 0;
 
-            // replace all parameters
-            try {
-                Pattern p = Pattern.compile("%s\\d?");
-                String[] msg_parts = p.split(str, -1);
-                Matcher m = p.matcher(str);
-                int nbparam = 0;
-                int order[] = new int[10];
-                boolean paramgenericorder = true;
-                while (m.find()) {
-                    String group = m.group();
-                    if (group.length() > 2) {
-                        if (nbparam != 0 && paramgenericorder) {
-                            // we got a mix of %s and %s\d, stick to %s only
-                        } else {
-                            paramgenericorder = false;
-                            int o = Integer.parseInt(group.substring(2));
-                            order[nbparam] = o;
-                        }
-                    } else {
-                        if (!paramgenericorder) {
-                            // we got a mix of %s and %s\d, stick to %s only
-                            paramgenericorder = true;
-                        }
-                    }
-                    nbparam++;
+            int order[] = null;
+            String msg_parts[] = null;
+            boolean paramgenericorder = true;
+
+            msg_parts = warningMessages.get(str);
+            if (msg_parts != null) {
+                order = warningOrder.get(str);
+                if (order != null) {
+                    paramgenericorder = false;
                 }
-                if (!paramgenericorder) {
-                    // let's do extra checks
-                    for (int i = 0; i < nbparam; i++) {
-                        if (order[i] > nbparam || order[i] == 0) {
-                            // too high or too low -> use %s only
-                            paramgenericorder = true;
-                            break;
+            } else {
+                // replace all parameters
+                try {
+                    Pattern p = Pattern.compile("%s\\d?");
+                    msg_parts = p.split(str, -1);
+                    Matcher m = p.matcher(str);
+                    int nbparam = 0;
+                    order = new int[10];
+                    paramgenericorder = true;
+                    while (m.find()) {
+                        String group = m.group();
+                        if (group.length() > 2) {
+                            if (nbparam != 0 && paramgenericorder) {
+                                // we got a mix of %s and %s\d, stick to %s only
+                            } else {
+                                paramgenericorder = false;
+                                int o = Integer.parseInt(group.substring(2));
+                                order[nbparam] = o;
+                            }
+                        } else {
+                            if (!paramgenericorder) {
+                                // we got a mix of %s and %s\d, stick to %s only
+                                paramgenericorder = true;
+                            }
                         }
-                        for (int j = i + 1; j < nbparam; j++) {
-                            if (order[i] == order[j]) {
-                                // two times the same value... -> use %s only
+                        nbparam++;
+                    }
+                    if (!paramgenericorder) {
+                        // let's do extra checks
+                        for (int i = 0; i < nbparam; i++) {
+                            if (order[i] > nbparam || order[i] == 0) {
+                                // too high or too low -> use %s only
                                 paramgenericorder = true;
                                 break;
                             }
+                            for (int j = i + 1; j < nbparam; j++) {
+                                if (order[i] == order[j]) {
+                                    // two times the same value... -> use %s only
+                                    paramgenericorder = true;
+                                    break;
+                                }
+                            }
+                            if (paramgenericorder) {
+                                break;
+                            }
                         }
-                        if (paramgenericorder) {
-                            break;
-                        }
+                        warningOrder.put(str, order);
                     }
+                    warningMessages.put(str, msg_parts);
+                } catch (PatternSyntaxException pex) {
                 }
-                int j = 0;
-                sb.append(msg_parts[0]);
-                for (int i = 1; i < msg_parts.length; i++) {
-                    if (j < args.length) {
-                        if (paramgenericorder) {
-                            sb.append(args[j++]);
-                        } else {
-                            sb.append(args[order[j++] - 1]);
-                        }
+            }
+            int j = 0;
+            sb.append(msg_parts[0]);
+            for (int i = 1; i < msg_parts.length; i++) {
+                if (j < args.length) {
+                    if (paramgenericorder) {
+                        sb.append(args[j++]);
                     } else {
-                        // TODO report error
-                        System.err.println("*** WARNING ISSUE: " + warning);
-                        System.err.println("*** WARNING ISSUE: " + ac.getMsg().getWarningString(warning));
-                        System.err.println("*** WARNING ISSUE: got " + args.length + " args entries");
+                        sb.append(args[order[j++] - 1]);
                     }
-                    sb.append(msg_parts[i]);
+                } else {
+                    // TODO report error
+                    System.err.println("*** WARNING ISSUE: " + warning);
+                    System.err.println("*** WARNING ISSUE: " + ac.getMsg().getWarningString(warning));
+                    System.err.println("*** WARNING ISSUE: got " + args.length + " args entries");
                 }
-            } catch (PatternSyntaxException pex) {
+                sb.append(msg_parts[i]);
             }
             return sb.toString();
         }
