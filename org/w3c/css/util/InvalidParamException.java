34dfc9f31d37179f4c60194276d1735df1e44dd3
Yves Lafon <ylafon@w3.org>
2020-09-16 16:58:06 +0200

    add support for ordering of %s tokens (using %s[1-9]) See #282

--- org/w3c/css/util/InvalidParamException.java
+++ org/w3c/css/util/InvalidParamException.java
@@ -9,6 +9,10 @@ package org.w3c.css.util;
 
 import org.w3c.css.parser.analyzer.ParseException;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 /**
  * @version $Revision$
  */
@@ -39,7 +43,7 @@ public class InvalidParamException extends ParseException {
      * @param message a message to add
      */
     public InvalidParamException(String error, Object message, ApplContext ac) {
-        super(processError(error, new String[] {(message != null) ? message.toString() : null}, ac));
+        super(processError(error, new String[]{(message != null) ? message.toString() : null}, ac));
         errorType = error;
     }
 
@@ -64,8 +68,8 @@ public class InvalidParamException extends ParseException {
     public InvalidParamException(String error, Object message1,
                                  Object message2, ApplContext ac) {
         super(processError(error, new String[]{
-                (message1 != null) ? message1.toString() : null,
-                (message2 != null) ? message2.toString() : null},
+                        (message1 != null) ? message1.toString() : null,
+                        (message2 != null) ? message2.toString() : null},
                 ac));
         errorType = error;
     }
@@ -90,14 +94,64 @@ public class InvalidParamException extends ParseException {
             return "can't find the error message for " + error;
         }
         // replace all parameters
-        String[] msg_parts = str.split("%s", -1);
-        int j = 0;
-        sb.append(msg_parts[0]);
-        for (int i = 1; i < msg_parts.length; i++) {
-            if (j < args.length) {
-                sb.append(args[j++]);
+        try {
+            Pattern p = Pattern.compile("%s\\d?");
+            String[] msg_parts = p.split(str, -1);
+            Matcher m = p.matcher(str);
+            int nbparam = 0;
+            int order[] = new int[10];
+            boolean paramgenericorder = true;
+            while (m.find()) {
+                String group = m.group();
+                if (group.length() > 2) {
+                    if (nbparam != 0 && paramgenericorder) {
+                        // we got a mix of %s and %s\d, stick to %s only
+                    } else {
+                        paramgenericorder = false;
+                        int o = Integer.parseInt(group.substring(2));
+                        order[nbparam] = o;
+                    }
+                } else {
+                    if (!paramgenericorder) {
+                        // we got a mix of %s and %s\d, stick to %s only
+                        paramgenericorder = true;
+                    }
+                }
+                nbparam++;
             }
-            sb.append(msg_parts[i]);
+            if (!paramgenericorder) {
+                // let's do extra checks
+                for (int i = 0; i < nbparam; i++) {
+                    if (order[i] > nbparam || order[i] == 0) {
+                        // too high or too low -> use %s only
+                        paramgenericorder = true;
+                        break;
+                    }
+                    for (int j = i + 1; j < nbparam; j++) {
+                        if (order[i] == order[j]) {
+                            // two times the same value... -> use %s only
+                            paramgenericorder = true;
+                            break;
+                        }
+                    }
+                    if (paramgenericorder) {
+                        break;
+                    }
+                }
+            }
+            int j = 0;
+            sb.append(msg_parts[0]);
+            for (int i = 1; i < msg_parts.length; i++) {
+                if (j < args.length) {
+                    if (paramgenericorder) {
+                        sb.append(args[j++]);
+                    } else {
+                        sb.append(args[order[j++]-1]);
+                    }
+                }
+                sb.append(msg_parts[i]);
+            }
+        } catch (PatternSyntaxException pex) {
         }
         return sb.toString();
     }
