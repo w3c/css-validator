73960ac874968ed46ee3d1d73c3ec3551190d83b
Yves Lafon <ylafon@w3.org>
2021-02-01 11:32:41 +0100

    rewrote deprecated API use

--- org/w3c/css/properties/tv/CssListStyle.java
+++ org/w3c/css/properties/tv/CssListStyle.java
@@ -114,7 +114,7 @@ public class CssListStyle extends org.w3c.css.properties.css.CssListStyle {
             }
             if (op != SPACE) {
                 throw new InvalidParamException("operator",
-                        ((new Character(op)).toString()), ac);
+                        Character.toString(op), ac);
             }
             expression.next();
         }
