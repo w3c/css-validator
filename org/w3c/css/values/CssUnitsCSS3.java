c8a155c722246cc23ed0ae7d2507344b030e4e50
Yves Lafon <ylafon@w3.org>
2021-10-21 12:20:56 +0200

    new unit per https://www.w3.org/TR/2021/WD-css-values-4-20211016/

--- org/w3c/css/values/CssUnitsCSS3.java
+++ org/w3c/css/values/CssUnitsCSS3.java
@@ -11,15 +11,18 @@ import org.w3c.css.util.InvalidParamException;
 import java.math.BigDecimal;
 
 /**
- * @spec http://www.w3.org/TR/2012/CR-css3-values-20120828/
+ * @spec https://www.w3.org/TR/2021/WD-css-values-4-20211016/
  * @spec http://www.w3.org/TR/2012/CR-css3-speech-20120320/
  * @spec https://www.w3.org/TR/2016/CR-css-grid-1-20160929/#typedef-flex
  * @spec https://github.com/w3c/csswg-drafts/blob/642e0f0b369c95619e6398788a943301656a89b8/css-values-4/Overview.bs
- *       (for dimension 'x')
+ * (for dimension 'x')
  */
 public class CssUnitsCSS3 {
     private static final String[] relative_length_units = {
-            "em", "ex", "ch", "rem", "vw", "vh", "vmin", "vmax"
+            "em", "rem", "ex", "rex", "cap", "rcap", "ch", "rch", "ic", "ric", "lh", "rlh",
+            "vw", "svw", "lvw", "dvw", "vh", "svh", "lvh", "dvh",
+            "vi", "svi", "lvi", "dvi", "vb", "svb", "lvb", "dvb",
+            "vmin", "svmin", "lvmin", "dvnim", "vmax", "svmax", "lvmax", "dvmax"
     };
 
     private static final String[] absolute_length_units = {
