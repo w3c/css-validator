2da6d8c5417c5cd71a17f0105baaf0eee5eee931
Yves Lafon <ylafon@w3.org>
2026-09-07 17:04:30 -0700

    udpated object-fit per https://www.w3.org/TR/2025/WD-css-images-4-20250930/#the-object-fit

--- org/w3c/css/properties/css3/CssObjectFit.java
+++ org/w3c/css/properties/css3/CssObjectFit.java
@@ -9,23 +9,48 @@ import org.w3c.css.util.ApplContext;
 import org.w3c.css.util.InvalidParamException;
 import org.w3c.css.values.CssExpression;
 import org.w3c.css.values.CssIdent;
+import org.w3c.css.values.CssImage;
 import org.w3c.css.values.CssTypes;
 import org.w3c.css.values.CssValue;
+import org.w3c.css.values.CssValueList;
+
+import java.util.ArrayList;
+
+import static org.w3c.css.values.CssOperator.SPACE;
 
 /**
- * @spec https://www.w3.org/TR/2020/CRD-css-images-3-20201217/#propdef-object-fit
+ * @spec https://www.w3.org/TR/2025/WD-css-images-4-20250930/#the-object-fit
  */
 public class CssObjectFit extends org.w3c.css.properties.css.CssObjectFit {
 
-    public static final CssIdent[] allowed_values;
+    public static final CssIdent[] allowed_values, allowed_unique_values;
+    public static final CssValue scale_down, contain, cover;
 
     static {
         String[] _allowed_values = {"fill", "contain", "cover", "none", "scale-down"};
+        String[] _allowed_unique_values = {"fill", "none"};
         allowed_values = new CssIdent[_allowed_values.length];
+        allowed_unique_values = new CssIdent[_allowed_unique_values.length];
         int i = 0;
         for (String s : _allowed_values) {
             allowed_values[i++] = CssIdent.getIdent(s);
         }
+        i = 0;
+        for (String s : _allowed_unique_values) {
+            allowed_unique_values[i++] = CssIdent.getIdent(s);
+        }
+        scale_down = CssIdent.getIdent("scale-down");
+        contain = CssIdent.getIdent("contain");
+        cover = CssIdent.getIdent("cover");
+    }
+
+    public static CssIdent getAllowedUniqueIdent(CssIdent ident) {
+        for (CssIdent id : allowed_unique_values) {
+            if (id.equals(ident)) {
+                return id;
+            }
+        }
+        return null;
     }
 
     public static CssIdent getAllowedIdent(CssIdent ident) {
@@ -53,30 +78,59 @@ public class CssObjectFit extends org.w3c.css.properties.css.CssObjectFit {
      */
     public CssObjectFit(ApplContext ac, CssExpression expression, boolean check)
             throws InvalidParamException {
-        if (check && expression.getCount() > 1) {
+        if (check && expression.getCount() > 2) {
             throw new InvalidParamException("unrecognize", ac);
         }
         setByUser();
 
+        ArrayList<CssValue> values = new ArrayList<CssValue>();
         CssValue val;
         char op;
+        boolean got_scale_down = false;
+        boolean got_contain = false;
 
-        val = expression.getValue();
-        op = expression.getOperator();
+        while (!expression.end()) {
+            val = expression.getValue();
+            op = expression.getOperator();
 
-        if (val.getType() != CssTypes.CSS_IDENT) {
-            throw new InvalidParamException("value",
-                    val.toString(),
-                    getPropertyName(), ac);
+            if (val.getType() != CssTypes.CSS_IDENT) {
+                throw new InvalidParamException("value",
+                        val.toString(),
+                        getPropertyName(), ac);
+            }
+            CssIdent ident = val.getIdent();
+            if (CssImage.isVerticalIdent(ident) || getAllowedUniqueIdent(ident) != null) {
+                if (expression.getCount() > 1) {
+                    throw new InvalidParamException("value", val.toString(),
+                            getPropertyName(), ac);
+                }
+                values.add(val);
+                break;
+            }
+            if (!CssIdent.isCssWide(ident) && getAllowedIdent(ident) == null) {
+                throw new InvalidParamException("value",
+                        val.toString(),
+                        getPropertyName(), ac);
+            }
+            value = val;
+            if (!got_scale_down && scale_down.equals(ident)) {
+                got_scale_down = true;
+                values.add(val);
+            } else if (!got_contain && (contain.equals(ident) || cover.equals(ident))) {
+                got_contain = true;
+                values.add(val);
+            } else {
+                throw new InvalidParamException("value",
+                        val.toString(),
+                        getPropertyName(), ac);
+            }
+            if (op != SPACE) {
+                throw new InvalidParamException("operator", op,
+                        getPropertyName(), ac);
+            }
+            expression.next();
         }
-        CssIdent ident = val.getIdent();
-        if (!CssIdent.isCssWide(ident) && getAllowedIdent(ident) == null) {
-            throw new InvalidParamException("value",
-                    val.toString(),
-                    getPropertyName(), ac);
-        }
-        value = val;
-
+        value = (values.size() == 1) ? values.get(0) : new CssValueList(values);
         expression.next();
     }
 
